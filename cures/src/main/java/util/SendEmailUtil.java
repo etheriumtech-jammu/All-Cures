5513ed30f3287b6ebe850e4af5db6c925546b9c1
Anil Raina <anilraina001@gmail.com>
2021-09-09 13:38:02 +0530

    Email properties read from cures.properties file.

--- cures/src/main/java/util/SendEmailUtil.java
+++ cures/src/main/java/util/SendEmailUtil.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -12,97 +15,94 @@ import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.server.ServletServerHttpAsyncRequestControl;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.servlet.support.RequestContext;
 
 import model.Registration;
 
 public class SendEmailUtil {
-	
+
 //	@Autowired
 //	static    HttpServletRequest request; 
-	public static String shootEmail(String to, String subject, String messageText) {
+	public static String shootEmail(String to, String subject, String messageText) throws IOException {
 
 		// Recipient's email ID needs to be mentioned.
 		// String to = "anilraina@etheriumtech.com";
 		if (null == to) {
-		    //HttpServletRequest req = (HttpServletRequest) request;
-			HttpServletRequest request = 
-					((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+			// HttpServletRequest req = (HttpServletRequest) request;
+			HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
 					.getRequest();
 			HttpSession session = request.getSession(true);
-			
+
 			int reg_id = 0;
 			if (session.getAttribute(Constant.USER) != null) {
 				Constant.log("Email UTIL#########USER IS IN SESSION########", 0);
 				Registration user = (Registration) session.getAttribute(Constant.USER);
 				reg_id = user.getRegistration_id();
-				System.out.println(reg_id); 
-				to = user.getEmail_address();	
+				System.out.println(reg_id);
+				to = user.getEmail_address();
 			}
-			 
 		}
 
-		// Sender's email ID needs to be mentioned
-		String from = "anilraina@etheriumtech.com";
+		// FileReader reader = new FileReader("cures.properties");
+		String propFileName = "cures.properties";
+		InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(propFileName);
+		final Properties prop = new Properties();
 
-		// Assuming you are sending email from through gmails smtp
-		String host = "smtpout.secureserver.net";
+		if (inputStream != null) {
+			prop.load(inputStream);
+		} else {
+			throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+		}
 
 		// Get system properties
 		Properties properties = System.getProperties();
 
 		// Setup mail server
-		properties.put("mail.smtp.host", host);
-		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.host", prop.getProperty("SMTP_HOST"));
+		properties.put("mail.smtp.port", prop.getProperty("SMTP_PORT"));
 		properties.put("mail.smtp.ssl.enable", "true");
 		properties.put("mail.smtp.auth", "true");
 		// properties.put("mail.smtp.starttls.enable", "true");
 
 		// Session session = Session.getInstance(properties);
+		EnDeCryptor aesEncryptionDecryption = new EnDeCryptor();
+		final String secretKey = Constant.SECRETE;
+		final String decryptedString = aesEncryptionDecryption.decrypt(prop.getProperty("SMTP_EMAIL_PASS_ENC"),
+				secretKey);
 
 		// Get the Session object.// and pass username and password
 		Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-
 			protected PasswordAuthentication getPasswordAuthentication() {
-
-				return new PasswordAuthentication("anilraina@etheriumtech.com", "password123");
-
+				return new PasswordAuthentication(prop.getProperty("SMTP_EMAIL_FROM_INFO"), decryptedString);
 			}
-
 		});
 
 		// Used to debug SMTP issues
-		session.setDebug(true);
+		session.setDebug(false);
 
 		try {
 			// Create a default MimeMessage object.
 			MimeMessage message = new MimeMessage(session);
-
 			// Set From: header field of the header.
-			message.setFrom(new InternetAddress(from));
-
+			message.setFrom(new InternetAddress(prop.getProperty("SMTP_EMAIL_FROM_INFO")));
 			// Set To: header field of the header.
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
 			// Set Subject: header field
 			message.setSubject(subject);
-
 			// Now set the actual message
 			message.setText(messageText);
-
-			System.out.println("sending...");
+			System.out.println("sending Email...");
 			// Send message
 			Transport.send(message);
 			System.out.println("Sent message successfully....");
 		} catch (MessagingException mex) {
 			mex.printStackTrace();
+			return "Error in email sending.";
+
 		}
 
-		return "Email sent";
+		return "Email sent successfully";
 
 	}
 
