cc9aefd01e8d7e682891f11f4dc92664838255b7
Anil Raina <anilraina001@gmail.com>
2021-11-15 17:47:57 +0530

    uploadFile and fetchUrl added for editor.js sync

--- cures/src/main/java/controller/ArticleController.java
+++ cures/src/main/java/controller/ArticleController.java
@@ -1,5 +1,16 @@
 package controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,8 +23,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import dao.ArticleDaoImpl;
 import model.Article;
@@ -25,6 +39,8 @@ public class ArticleController {
 
 	@Autowired
 	private ArticleDaoImpl articleDaoImpl;
+
+	private String saveDirectory = "C:/test/";
 
 	@RequestMapping(value = "/{article_id}", produces = "application/json", method = RequestMethod.GET)
 	public @ResponseBody Article_dc_name getArticleDetails(@PathVariable int article_id, HttpServletRequest request) {
@@ -63,6 +79,66 @@ public class ArticleController {
 	@RequestMapping(value = "/{article_id}", produces = "application/json", method = RequestMethod.DELETE)
 	public @ResponseBody int deleteArticle(@PathVariable int article_id) {
 		return articleDaoImpl.deleteArticleId(article_id);
+	}
+
+	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST, produces = "application/json")
+	public @ResponseBody HashMap uploadFile(@RequestParam CommonsMultipartFile image, HttpServletRequest request,
+			HttpSession session) {
+		String path = session.getServletContext().getRealPath("/uitest");
+		System.out.println(path);
+		// path = path+"/uitest";
+		String filename = image.getOriginalFilename();
+
+		System.out.println(path + " " + filename);
+		try {
+			byte barr[] = image.getBytes();
+
+			BufferedOutputStream bout = new BufferedOutputStream(new FileOutputStream(path + "/" + filename));
+			bout.write(barr);
+			bout.flush();
+			bout.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		HashMap hm = new HashMap();
+		hm.put("success", 1);
+		HashMap hm2 = new HashMap();
+		String baseUrl = ServletUriComponentsBuilder.fromRequestUri(request).replacePath(null).build().toUriString();
+
+		System.out.println(baseUrl);
+		hm2.put("url", baseUrl + "/cures/uitest/" + filename);
+		hm.put("file", hm2);
+		return hm;
+	}
+
+	@RequestMapping(value = "/fetchUrl", method = RequestMethod.POST, produces = "application/json")
+	public @ResponseBody HashMap fetchUrl(@RequestBody HashMap dataMap, HttpServletRequest request, HttpSession session)
+			throws MalformedURLException {
+		String url = (String) dataMap.get("url");
+		URL urlExt = new URL(url);
+
+		String path = session.getServletContext().getRealPath("/uitest");
+		String filename = urlExt.toString().substring(urlExt.toString().lastIndexOf('/') + 1,
+				urlExt.toString().length());
+		System.out.println(path + " " + filename);
+		try {
+			try (InputStream in = urlExt.openStream()) {
+				Files.copy(in, Paths.get(path + File.separator + filename));
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		HashMap hm = new HashMap();
+		hm.put("success", 1);
+		HashMap hm2 = new HashMap();
+		String baseUrl = ServletUriComponentsBuilder.fromRequestUri(request).replacePath(null).build().toUriString();
+
+		System.out.println(baseUrl);
+		hm2.put("url", baseUrl + "/cures/uitest/" + filename);
+		hm.put("file", hm2);
+		return hm;
 	}
 
 }
