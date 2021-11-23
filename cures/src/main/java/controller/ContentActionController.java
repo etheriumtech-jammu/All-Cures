21771c5c1d7100365acd558345aa93524fef2fa8
Anil Raina <anilraina001@gmail.com>
2021-11-23 16:18:56 +0530

    request object changed to jsonobject as UI canged to axios with json with request bodyu

--- cures/src/main/java/controller/ContentActionController.java
+++ cures/src/main/java/controller/ContentActionController.java
@@ -11,14 +11,17 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
 import dao.ContentDaoImpl;
@@ -56,36 +59,39 @@ public class ContentActionController extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
     
-    public int createArticle(HttpServletRequest request, HttpServletResponse response, Registration user){
-
-    	String title= request.getParameter("title");
+    public int createArticle(HttpServletRequest request, HttpServletResponse response, Registration user) throws IOException{
+		String requestJsonStr = IOUtils.toString(request.getInputStream(), "UTF-8");
+		ObjectMapper mapper = new ObjectMapper();
+    	Map<String,Object> requestJsonMap = mapper.readValue(requestJsonStr, Map.class);
+    	
+    	String title= (String) requestJsonMap.get("title");
     	Constant.log("Creating Article with Title:"+title, 1);
-    	String artFrndlyNm= request.getParameter("friendlyName");
+    	String artFrndlyNm= (String) requestJsonMap.get("friendlyName");
     	Constant.log("Creating Article with Friendly Name:"+artFrndlyNm, 0);
-		String lang= request.getParameter("language");
-		Constant.log("strLanguage:"+lang, 0);
-		int iLang = 1; //English By Default
-		if(lang != null && !"".equals(lang.trim())){
-			iLang = Integer.parseInt(lang);
-			Constant.log("intLanguage:"+iLang, 0);
-		}
-		Constant.log("Creating Article with Language:"+lang, 0);
-		String articleContent= request.getParameter("articleContent");
+		int iLang= (int) requestJsonMap.get("language");
+		Constant.log("strLanguage:"+iLang, 0);
+//		int iLang = 1; //English By Default
+//		if(lang != null && !"".equals(lang.trim())){
+//			iLang = Integer.parseInt(lang);
+//			Constant.log("intLanguage:"+iLang, 0);
+//		}
+//		Constant.log("Creating Article with Language:"+lang, 0);
+		String articleContent= (String) requestJsonMap.get("articleContent");
 			//TODO: Capture Metadata of the Article and Persist in backend
-		String subHead=request.getParameter("subHeading");
+		String subHead=(String) requestJsonMap.get("subHeading");
 		Constant.log("Creating Article with subHeading:"+subHead, 0);
 		//TODO: Need to create FK constraint in the DB for Content Types
-		String content_type=request.getParameter("contentType");
+		String content_type=(String) requestJsonMap.get("contentType");
 //		int iContentType = 1; //Article By Default
 //		if(content_type != null && !"".equals(content_type.trim())){
 //			iContentType = Integer.parseInt(content_type);
 //		}
 		Constant.log("Creating Article with Content Type:"+content_type, 0);
 		
-		String type=request.getParameter("type");
+		String type=(String) ""+requestJsonMap.get("type");
 		Constant.log("Creating Article with Type:"+type, 0);
 
-		String status= request.getParameter("articleStatus");
+		String status= (String) requestJsonMap.get("articleStatus");
 		System.out.println("Test articleStatus Val"+status);
 		int iStatus = 1; //WIP By Default
 		if(status  != null && !"".equals(status.trim())){
@@ -93,45 +99,45 @@ public class ContentActionController extends HttpServlet {
 		}
 		Constant.log("Creating Article with Status:"+status, 0);
 		//disclaimer (should be an id)
-		String d_loc=request.getParameter("disclaimerId");
-		int iDiscId = -1; //Negative indicates error
-		if(d_loc != null && !"".equals(d_loc.trim())){
-			iDiscId = Integer.parseInt(d_loc);
-		}
-		Constant.log("Creating Article with Disclaimer:"+d_loc, 0);
+		int iDiscId=(int) requestJsonMap.get("disclaimerId");
+//		int iDiscId = -1; //Negative indicates error
+//		if(d_loc != null && !"".equals(d_loc.trim())){
+//			iDiscId = Integer.parseInt(d_loc);
+//		}
+		Constant.log("Creating Article with Disclaimer:"+iDiscId, 0);
 		//copyright (should be an id)
-		String c_loc=request.getParameter("copyId");;
-		int iCopyId = -1; //Negative indicates error
-		if(c_loc != null && !"".equals(c_loc.trim())){
-			iCopyId = Integer.parseInt(c_loc);
-		}
-		Constant.log("Creating Article with Copyright:"+c_loc, 0);
+		int iCopyId=(int) requestJsonMap.get("copyId");;
+//		int iCopyId = -1; //Negative indicates error
+//		if(c_loc != null && !"".equals(c_loc.trim())){
+//			iCopyId = Integer.parseInt(c_loc);
+//		}
+		Constant.log("Creating Article with Copyright:"+iCopyId, 0);
 		
-		String authIdS = request.getParameter("authById");;
+		String authIdS = (String) ""+requestJsonMap.get("authById");;
 //		int iAuthId = -1; //Negative indicates error
 //		if(authId != null && !"".equals(authId.trim())){
 //			iAuthId = Integer.parseInt(authId);
 //		}
 		Constant.log("Creating Article with Author:"+authIdS, 0);
-		String keyword=request.getParameter("keywords");
-		String window_title=request.getParameter("winTitle");;
-		String articlecontent= (String) request.getParameter("articleContent");
-		System.out.println("##############RAINA###"+articlecontent);
-		String countryId = request.getParameter("countryId");;
+		String keyword=(String) requestJsonMap.get("keywords");
+		String window_title=(String) requestJsonMap.get("winTitle");;
+		String articlecontent= (String) (String) requestJsonMap.get("articleContent");
+		System.out.println("##############articlecontent###"+articlecontent);
+		String countryId = (String) requestJsonMap.get("countryId");;
 		int iCountryId = -1; //Negative indicates error
 		if(countryId != null && !"".equals(countryId.trim())){
 			iCountryId = Integer.parseInt(countryId);
 		}
 		Constant.log("Creating Article with Authors:"+authIdS, 0);
 		
-		String diseaseConditionId = request.getParameter("diseaseConditionId");;
+		String diseaseConditionId = (String) requestJsonMap.get("diseaseConditionId");;
 		int iDiseaseConditionId = -1; //Negative indicates error
 		if(diseaseConditionId != null && !"".equals(diseaseConditionId.trim())){
 			iDiseaseConditionId = Integer.parseInt(diseaseConditionId);
 		}
 		Constant.log("Creating Article with Author:"+diseaseConditionId, 0);
 		
-		String promoId = request.getParameter("promoId");;
+		String promoId = (String) requestJsonMap.get("promoId");;
 		int ipromoId = -1; //Negative indicates error
 		int promoStage = -1; //Negative indicates error
 		if(promoId != null && !"".equals(promoId.trim())){
@@ -140,7 +146,7 @@ public class ContentActionController extends HttpServlet {
 		}
 		Constant.log("Creating Article with promoId:"+promoId, 0);
 		Constant.log("Saving Content in Dao", 1);
-		String comments= request.getParameter("comments");
+		String comments= (String) requestJsonMap.get("comments");
 		Constant.log("comments:"+comments, 0);
 		//User Object in Session is coming Null Here and Causing a Null Pointer Exception on the next line
 		//This should never be the case as the user has to be logged  in to create an article
