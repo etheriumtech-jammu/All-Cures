59c7d43e37e795894dd7e66c53d0f4eea513d32d
Anil Raina <anilraina001@gmail.com>
2021-12-16 20:01:25 +0530

    Test and check if request is sent from interakt to this api

--- cures/src/main/java/controller/WebHookWhatsAPIController.java
+++ cures/src/main/java/controller/WebHookWhatsAPIController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,25 @@ public class WebHookWhatsAPIController {
 
 	@PostMapping("/apisuccess")
 	public ResponseEntity<String> paymentSuccess(HttpServletRequest request) throws Exception {
-		System.out.println("<<<<<<<<<<<<<<<<<<IN apisuccess 1st line>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println("<<<<<<<<<<<<<<<<<<IN apisuccess 1st line>>>>>>>>>>>>>>>>>>>>>>>>>>>POST");
 
-		String key_secret_verify = interaktwebookKey;
+		String key_secret_verify = "ZWHYR/5vDZI/xyBKDQe6ew==";//interaktwebookKey;
+		// ZWHYR/5vDZI/xyBKDQe6ew==
+		if (!key_secret_verify.equals((String)request.getParameter("secretKEYinterakt"))) {
+			return new ResponseEntity<String>(HttpStatus.FORBIDDEN);
+		}
+
+		// do whatever you want to do with the valid paddle request
+		System.out.println(request.getParameterMap());
+		return new ResponseEntity<String>(HttpStatus.OK);
+	}
+	
+
+	@GetMapping("/apisuccess")
+	public ResponseEntity<String> paymentSuccessGet(HttpServletRequest request) throws Exception {
+		System.out.println("<<<<<<<<<<<<<<<<<<IN apisuccess 1st line>>>>>>>>>>>>>>>>>>>>>>>>>>>GET");
+
+		String key_secret_verify = "ZWHYR/5vDZI/xyBKDQe6ew==";//interaktwebookKey;
 		// ZWHYR/5vDZI/xyBKDQe6ew==
 		if (!key_secret_verify.equals((String)request.getParameter("secretKEYinterakt"))) {
 			return new ResponseEntity<String>(HttpStatus.FORBIDDEN);
