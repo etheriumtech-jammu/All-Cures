5e0b2681a6db23a0450b5ea5b39a6c0308dc41dc
Anil Raina <anilraina001@gmail.com>
2022-02-04 14:47:18 +0530

    session ThreadLocal used

--- cures/src/main/java/util/HibernateUtil.java
+++ cures/src/main/java/util/HibernateUtil.java
@@ -11,36 +11,57 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
  * @author Anil Raina
  */
 public class HibernateUtil {
-  private static StandardServiceRegistry standardServiceRegistry;
-  private static SessionFactory sessionFactory;
+	private static StandardServiceRegistry standardServiceRegistry;
+	private static SessionFactory sessionFactory;
+	private static ThreadLocal<Session> threadLocal = new ThreadLocal<Session>();
+	static {
+		if (sessionFactory == null) {
+			try {
+				// Create StandardServiceRegistry
+				standardServiceRegistry = new StandardServiceRegistryBuilder().configure().build();
+				// Create MetadataSources
+				MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
+				// Create Metadata
+				Metadata metadata = metadataSources.getMetadataBuilder().build();
+				// Create SessionFactory
+				sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-  static{
-	    if (sessionFactory == null) {
-	      try {
-	        // Create StandardServiceRegistry
-	        standardServiceRegistry = new StandardServiceRegistryBuilder()
-	            .configure()
-	            .build();
-	        // Create MetadataSources
-	        MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
-	        // Create Metadata
-	        Metadata metadata = metadataSources.getMetadataBuilder().build();
-	        // Create SessionFactory
-	        sessionFactory = metadata.getSessionFactoryBuilder().build();
-	      } catch (Exception e) {
-	        e.printStackTrace();
-	        if (standardServiceRegistry != null) {
-	          StandardServiceRegistryBuilder.destroy(standardServiceRegistry);
-	        }
-	      }
-	    }
-  }
-  //Utility method to return SessionFactory
-  public static SessionFactory getSessionFactory() {
-	  return sessionFactory;
-  }
-  
+				threadLocal = new ThreadLocal<Session>();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				if (standardServiceRegistry != null) {
+					StandardServiceRegistryBuilder.destroy(standardServiceRegistry);
+				}
+			}
+		}
+	}
+
+	// Utility method to return SessionFactory
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
 	public static Session buildSessionFactory() {
-		return sessionFactory.openSession();
+		Session session = threadLocal.get();
+		if (session == null) {
+			session = sessionFactory.openSession();
+			threadLocal.set(session);
+		}
+		return session;
+//		return sessionFactory.openSession();
+	}
+
+	public static void closeSession() {
+		Session session = threadLocal.get();
+		if (session != null) {
+			session.close();
+			threadLocal.set(null);
+		}
+	}
+
+	public static void closeSessionFactory() {
+		sessionFactory.close();
+		StandardServiceRegistryBuilder.destroy(standardServiceRegistry);
 	}
 }
