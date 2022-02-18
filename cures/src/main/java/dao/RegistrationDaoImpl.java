505a3d3d4c2330c47a6326487e8a975be38be34d
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2022-02-18 16:58:49 +0530

    change obj value of rowno to 12

--- cures/src/main/java/dao/RegistrationDaoImpl.java
+++ cures/src/main/java/dao/RegistrationDaoImpl.java
@@ -205,7 +205,7 @@ public class RegistrationDaoImpl {
 				register.setRemember_me(obj[9] != null ? (Integer) obj[9] : 0);
 				register.setLogin_attempt(obj[10] != null ? (Integer) obj[10] : 0);
 				register.setLast_login_datatime((java.util.Date) obj[11]);
-				register.setRowno(obj[10] != null ? (Long) obj[12] : 0);
+				register.setRowno(obj[12] != null ? (Long) obj[12] : 0);
 				Constant.log(Constant.PREFIX + obj[0], 0);
 				Constant.log(Constant.FIRST_NAME + obj[1], 0);
 			}
