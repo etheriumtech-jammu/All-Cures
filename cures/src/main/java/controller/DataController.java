1cd6d1431d5579482fb4b55c5677e4a8d9801eb4
arnavkoul-cyber <69846798+arnavkoul-cyber@users.noreply.github.com>
2022-03-21 17:01:01 +0530

    new api for data capture

---
+++ cures/src/main/java/controller/DataController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import dao.DataDaoImpl;
+
+@RestController
+@RequestMapping(path = "/data")
+public class DataController {
+
+	@Autowired
+	private DataDaoImpl dataDaoImpl;
+
+	
+
+	@RequestMapping(value = "/create", produces = "application/json", method = RequestMethod.POST)
+	public @ResponseBody int addDataDetails(@RequestBody HashMap dataMasterMap) {
+		return dataDaoImpl.addDataDetails(dataMasterMap);
+	}
+
+
+	
+	
+
+}
---
+++ cures/src/main/java/dao/DataDaoImpl.java
@@ -0,0 +1,62 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Component;
+
+import util.HibernateUtil;
+
+@Component
+public class DataDaoImpl {
+
+	
+	public static int addDataDetails(HashMap dataMap) {
+
+		// creating seession factory object
+		Session session = HibernateUtil.buildSessionFactory();
+
+		// creating session object
+		//Session session = factory;
+		// creating transaction object
+		session.beginTransaction();
+
+		int iemi = (int) dataMap.get("iemi");
+		String search = null;
+		int user_id = (int) dataMap.get("user_id");
+//		int date = (int) dataMap.get("date");;
+		
+
+		Query query = session
+				.createNativeQuery("INSERT INTO `data_store`\r\n" + " (`iemi`,"
+						+ " `search`," + " `user_id`," 
+						+ " VALUES" + " ('" + iemi + "'," + " '" + search + "'," + " '"
+						+ " '" + user_id + "',);" + "");
+		// needs other condition too but unable to find correct column
+		int ret = 0;
+		try {
+			ret = query.executeUpdate();
+			session.getTransaction().commit();
+			System.out.println("inserted new entry to data_store table for iemi =  " + iemi);
+
+		} catch (Exception ex) {
+			session.getTransaction().rollback();
+		} finally {
+			// session.getTransaction().commit();   //session.close();
+//			session.getTransaction().commit();   //session.close();
+		}
+		// session.getTransaction().commit();   //session.close();
+
+		return ret;
+	}
+
+	
+	
+
+
+
+	}
