c941da48d4add40f094c26e5b868026d6d1bb15c
Divya Sharma <115102098+DivuShubhu@users.noreply.github.com>
2022-11-23 12:29:42 +0530

    Add files via upload

--- cures/src/main/java/dao/ip_detaildao.java
+++ cures/src/main/java/dao/ip_detaildao.java
@@ -4,9 +4,11 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -15,34 +17,40 @@ import org.hibernate.cfg.Configuration;
 import org.springframework.util.StringUtils;
 
 import model.*;
+import util.Constant;
 import util.HibernateUtil;
 
 public class ip_detaildao {
 
-	public static int Insert(Integer id, String add,Integer reg) {
+	public static Integer Insert(Integer article_id, HttpServletRequest request,Map<String,String> headers,Integer user_id) {
 	
 		SessionFactory factory=new Configuration().configure().buildSessionFactory();
 	//	Session session = HibernateUtil.buildSessionFactory();
 		//Transaction transaction = session.getTransaction();
 	//	session.beginTransaction();
-		System.out.println(add);
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession(true);
+	
+		String cookie1=headers.get("cookie");
+		
+		
 		
 		 SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
 	        Date date = new Date();  
 		IP_Details ip =new IP_Details();
-		 ip.setArticle_id(id);
+		 ip.setArticle_id(article_id);
 		 ip.setDate(formatter.format(date));
-		 ip.setIp_address(add);
-		 ip.setReg_id(reg);
-	       Session session=factory.openSession();
-	       Transaction tx=session.beginTransaction();
-	     session.save(ip);
+		 ip.setIp_address(cookie1);
+		 ip.setReg_id(user_id);
+	       Session session1=factory.openSession();
+	       Transaction tx=session1.beginTransaction();
+	     session1.save(ip);
 	       tx.commit();
 	    
-	       session.close();
+	       session1.close();
+		
 		return 1;
-		
-		
 		
 		
 	}
