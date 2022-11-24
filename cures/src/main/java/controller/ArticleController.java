ed32f732416d6163bd0ee16bfb50e6ce22c99984
Divya Sharma <115102098+DivuShubhu@users.noreply.github.com>
2022-11-24 16:14:45 +0530

    Update ArticleController.java

--- cures/src/main/java/controller/ArticleController.java
+++ cures/src/main/java/controller/ArticleController.java
@@ -63,10 +63,10 @@ public class ArticleController {
 
 	}
 	
-	@RequestMapping(value = "/{article_id}/{user_id}", produces = "application/json", method = RequestMethod.POST)
-	public @ResponseBody int  PostDetails(@PathVariable int article_id,@PathVariable int user_id, HttpServletRequest request,@RequestHeader Map<String,String> headers) {
+	@RequestMapping(value = "/{article_id}/{user_id}/{cookie}/{whats_app}", produces = "application/json", method = RequestMethod.POST)
+	public @ResponseBody int  PostDetails(@PathVariable int article_id,@PathVariable int user_id,@PathVariable String cookie,@PathVariable String whats_app, HttpServletRequest request,@RequestHeader Map<String,String> headers) {
 		
-	return	 ip_detaildao.Insert(article_id,request,headers,user_id);
+	return	 ip_detaildao.Insert(article_id,request,user_id,cookie,whats_app);
 		
 	}
 	
