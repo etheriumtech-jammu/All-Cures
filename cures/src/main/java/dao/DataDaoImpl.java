560c16d523f377ee614b173e8581a29134e923b9
Divya Sharma <115102098+DivuShubhu@users.noreply.github.com>
2022-12-29 12:57:07 +0530

    Update DataDaoImpl.java

--- cures/src/main/java/dao/DataDaoImpl.java
+++ cures/src/main/java/dao/DataDaoImpl.java
@@ -86,7 +86,37 @@ public class DataDaoImpl {
 
 	}
 	
-	
+	public static List viewMedicines() {
+		Session session = HibernateUtil.buildSessionFactory();
+		
+		Query query = session.createNativeQuery("SELECT * FROM medicinetype;");
+		
+		List<Object[]> results = (List<Object[]>) query.getResultList();
+		
+		List hmFinal = new ArrayList();
+		
+		for (Object[] objects : results) {
+			HashMap hm = new HashMap();
+			
+			Integer med_id = (Integer) objects[0];
+		String med_type = (String) objects[1];
+		
+		    hm.put("med_id",med_id);
+			hm.put("med_type",med_type);
+			hm.put("Image", "folder/"+ med_id+ ".png");
+			hmFinal.add(hm);
+			
+			
+			
+		}
+			
+		return hmFinal;
+
+	}
+
+
+
+
 
 
 
