6ff207bae6e339bc1adf1e0b41b3c4c5b64258fc
Divya Sharma <115102098+DivuShubhu@users.noreply.github.com>
2023-02-03 15:38:44 +0530

    Update DataController.java

--- cures/src/main/java/controller/DataController.java
+++ cures/src/main/java/controller/DataController.java
@@ -12,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import dao.DeleteDaoImpl;
+
 import dao.DataDaoImpl;
+import dao.DeleteDaoImpl;
 
 @RestController
 @RequestMapping(path = "/data")
