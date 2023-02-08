d650263a5a230900ab9567118cac0b4c9a88e08c
Divya Sharma <115102098+DivuShubhu@users.noreply.github.com>
2023-02-08 15:05:26 +0530

    Update DataController.java

--- cures/src/main/java/controller/DataController.java
+++ cures/src/main/java/controller/DataController.java
@@ -6,12 +6,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import dao.DataDaoImpl;
+import dao.DeleteDaoImpl;
 
 @RestController
 @RequestMapping(path = "/data")
@@ -25,6 +28,23 @@ public class DataController {
 	@RequestMapping(value = "/create", produces = "application/json", method = RequestMethod.POST)
 	public @ResponseBody int addDataDetails(@RequestBody HashMap dataMasterMap) {
 		return dataDaoImpl.addDataDetails(dataMasterMap);
+	}
+	
+	
+@RequestMapping(value = "/deactivate/{usr_id}", produces = "application/json", method = RequestMethod.POST)
+	public @ResponseBody int delete_update(@PathVariable Integer usr_id,@RequestParam(required = false) String reason) {
+		
+		System.out.println("Request to deactivate");
+		return DeleteDaoImpl.Delete_Update(usr_id,reason);
+	
+		
+	}
+	
+	@RequestMapping(value = "/delete/{email}", produces = "application/json", method = RequestMethod.GET)
+	public @ResponseBody String login_delete(@PathVariable String email) {
+		
+		
+		return DeleteDaoImpl.Login_Delete(email);		
 	}
 
 }
