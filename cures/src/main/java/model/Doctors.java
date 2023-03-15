5d000d27c724226dd12ba4353c95533c06aa2e72
Divya Sharma <115102098+DivuShubhu@users.noreply.github.com>
2023-03-15 11:46:30 +0530

    Update Doctors.java

--- cures/src/main/java/model/Doctors.java
+++ cures/src/main/java/model/Doctors.java
@@ -111,6 +111,16 @@ public class Doctors implements Serializable {
 	private Integer countries_code;
 	private Integer hospital_affliated_code;
 	private Long rowno;
+	
+	private Integer subscription;
+
+	public Integer getSubscription() {
+		return subscription;
+	}
+
+	public void setSubscription(Integer subscription) {
+		this.subscription = subscription;
+	}
 
 	private String website_url;
 
