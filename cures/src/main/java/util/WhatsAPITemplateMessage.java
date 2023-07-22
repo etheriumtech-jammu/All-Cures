4361ca301de3d8a2bf77bb67b801699369157f6f
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-07-22 21:03:03 +0530

    Update WhatsAPITemplateMessage.java

--- cures/src/main/java/util/WhatsAPITemplateMessage.java
+++ cures/src/main/java/util/WhatsAPITemplateMessage.java
@@ -209,7 +209,7 @@ public class WhatsAPITemplateMessage {
 
 	public static void POSTRequestTrackEventsByTip(String title)
 			throws SQLException, IOException {
-		String templateName="25feballcures";
+		String templateName="TipOfTheDay";
 //		ArrayList NSData = new WAPICommon().fetchDatabaseResultsForNewsletterByTip();
 //		for (int i = 0; i < NSData.size(); i++) {
 		for (int i = 0; i < 1; i++) {
@@ -235,8 +235,7 @@ public class WhatsAPITemplateMessage {
 		String header1_imgpath="https://uat.all-cures.com/tip Final.jpeg";
 		String countryCode = params[2];
 		String title=params[3];
-		String title2="second";
-		String title3="third";
+		
 		System.out.println(title3);
 		String fileProperties = "whatsapi.properties";
 		Properties prop = new WAPICommon().readPropertiesFile(fileProperties);
@@ -244,7 +243,7 @@ public class WhatsAPITemplateMessage {
 		final String POST_PARAMS = "{\"countryCode\": \"" + countryCode + "\", \"phoneNumber\": \"" + mobile + "\","
 				+ " \"type\": \"Template\"," + " \"template\": {\"name\": \"" + template_name
 				+ "\",\"languageCode\": \"en\"," + " \"headerValues\": [\"" + header1_imgpath + "\"],"
-				+ " \"bodyValues\": [ \"" + title + "\", \"" + title2 + "\", \"" + title3 + "\"]"
+				+ " \"bodyValues\": [ \"" + title + "\"]"
 				+ "}}";
 		System.out.println(POST_PARAMS);
 //		URL obj = new URL("https://api.interakt.ai/v1/public/track/events/");
