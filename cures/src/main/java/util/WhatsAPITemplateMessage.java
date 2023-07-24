7a80ba219d3f6f459e28339dd53ccc1f59fcbba2
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-07-24 20:49:00 +0530

    Update WhatsAPITemplateMessage.java

--- cures/src/main/java/util/WhatsAPITemplateMessage.java
+++ cures/src/main/java/util/WhatsAPITemplateMessage.java
@@ -240,11 +240,13 @@ public class WhatsAPITemplateMessage {
 		String fileProperties = "whatsapi.properties";
 		Properties prop = new WAPICommon().readPropertiesFile(fileProperties);
 		
-		final String POST_PARAMS = "{\"countryCode\": \"" + countryCode + "\", \"phoneNumber\": \"" + mobile + "\","
+/*		final String POST_PARAMS = "{\"countryCode\": \"" + countryCode + "\", \"phoneNumber\": \"" + mobile + "\","
 				+ " \"type\": \"Template\"," + " \"template\": {\"name\": \"" + template_name
 				+ "\",\"languageCode\": \"en\"," + " \"headerValues\": [\"" + header1_imgpath + "\"],"
 				+ " \"bodyValues\": [ \"" + title + "\"]"
 				+ "}}";
+*/
+		final String POST_PARAMS ="{     \"countryCode\": \"+91\",     \"phoneNumber\": \"7006268978\",     \"type\": \"Template\",     \"template\": {         \"name\": \"tipoftheday\",         \"languageCode\": \"en\",         \"headerValues\": [\"https://uat.all-cures.com/tip Final.jpeg\"],         \"bodyValues\": [\"An apple a day keeps the doctor away.\"]     } }";
 		
 		
 		System.out.println(POST_PARAMS);
