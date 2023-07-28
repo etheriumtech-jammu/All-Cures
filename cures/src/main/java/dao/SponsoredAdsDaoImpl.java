a1c1a6a6a36654769cc0bc8da40c03088e459700
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-07-28 13:41:26 +0530

    Update SponsoredAdsDaoImpl.java
    add update dao

--- cures/src/main/java/dao/SponsoredAdsDaoImpl.java
+++ cures/src/main/java/dao/SponsoredAdsDaoImpl.java
@@ -2,7 +2,9 @@ package dao;
 
 
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -162,6 +164,187 @@ public class SponsoredAdsDaoImpl {
 
 		return hmFinal;
 
+	}
+
+	public static int updateCompanyId(int CompanyID, HashMap companyMap) {
+
+		// creating seession factory object
+		Session session = HibernateUtil.buildSessionFactory();
+
+		// creating session object
+		//Session session = factory;
+		// creating transaction object
+		session.beginTransaction();
+		 Date date= Date.valueOf(LocalDate.now());
+		String updatestr = "";
+		if (companyMap.containsKey("CompanyName")) {
+			updatestr += "`CompanyName` = '" + companyMap.get("CompanyName") + "',\r\n";
+		
+		}
+		
+		if (companyMap.containsKey("CompanyWebsite")) {
+			updatestr += "`CompanyWebsite` = '" + companyMap.get("CompanyWebsite") + "',\r\n";
+		}
+		if (companyMap.containsKey("ContactPerson")) {
+			updatestr += "`ContactPerson` = '" + companyMap.get("ContactPerson") + "',\r\n";
+		}
+		if (companyMap.containsKey("Email")) {
+			updatestr += "`Email` = '" + companyMap.get("Email") + "',\r\n";
+		}
+		if (companyMap.containsKey("Phone")) {
+			updatestr += "`type` = '" + companyMap.get("type") + "',\r\n";
+		}
+		
+		if (companyMap.containsKey("Status")) {
+			updatestr += "`Status` = '" + companyMap.get("Status") + "',\r\n";
+		}
+		updatestr+="LastUpdateddate='"  + date +  "'  ";
+		System.out.println();
+		Query query = session.createNativeQuery(
+				"UPDATE `Companies`\r\n" + "SET\r\n" + updatestr + "WHERE `CompanyID` = " + CompanyID + ";");
+		int ret = 0;
+		try {
+		ret = query.executeUpdate();
+		session.getTransaction().commit();
+		System.out.println("updated  table for CompanyID =  " + CompanyID);
+	
+	}catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	return ret;
+	}
+	
+	public static int updateCampaignId(int CampaignID, HashMap campaignMap) {
+
+		// creating seession factory object
+		Session session = HibernateUtil.buildSessionFactory();
+
+		// creating session object
+		//Session session = factory;
+		// creating transaction object
+		session.beginTransaction();
+		 Date date= Date.valueOf(LocalDate.now());
+		String updatestr = "";
+		if (campaignMap.containsKey("CompanyID")) {
+			updatestr += "`CompanyID` = '" + campaignMap.get("CompanyID") + "',\r\n";
+		
+		}
+		
+		if (campaignMap.containsKey("CampaignName")) {
+			updatestr += "`CampaignName` = '" + campaignMap.get("CampaignName") + "',\r\n";
+		}
+		if (campaignMap.containsKey("StartDate")) {
+			updatestr += "`StartDate` = '" + campaignMap.get("StartDate") + "',\r\n";
+		}
+		if (campaignMap.containsKey("EndDate")) {
+			updatestr += "`EndDate` = '" + campaignMap.get("EndDate") + "',\r\n";
+		}
+		
+		if (campaignMap.containsKey("Status")) {
+			updatestr += "`Status` = '" + campaignMap.get("Status") + "',\r\n";
+		}
+		updatestr+="LastUpdateddate='"  + date +  "'  ";
+		System.out.println();
+		Query query = session.createNativeQuery(
+				"UPDATE `Campaign`\r\n" + "SET\r\n" + updatestr + "WHERE `CampaignID` = " + CampaignID + ";");
+		int ret = 0;
+		try {
+		ret = query.executeUpdate();
+		session.getTransaction().commit();
+		System.out.println("updated  table for CampaignID =  " + CampaignID);
+	
+	}catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	return ret;
+	}
+	
+	public static int updateCampaignAdsId(int AdID, HashMap campaignAdsMap) {
+
+		// creating seession factory object
+		Session session = HibernateUtil.buildSessionFactory();
+
+		// creating session object
+		//Session session = factory;
+		// creating transaction object
+		session.beginTransaction();
+		 Date date= Date.valueOf(LocalDate.now());
+		String updatestr = "";
+		if (campaignAdsMap.containsKey("CampaignID")) {
+			updatestr += "`CampaignID` = '" + campaignAdsMap.get("CampaignID") + "',\r\n";
+		
+		}
+		
+		if (campaignAdsMap.containsKey("DiseaseCondition")) {
+			updatestr += "`DiseaseCondition` = '" + campaignAdsMap.get("DiseaseCondition") + "',\r\n";
+		}
+		if (campaignAdsMap.containsKey("AdTypeID")) {
+			updatestr += "`AdTypeID` = '" + campaignAdsMap.get("AdTypeID") + "',\r\n";
+		}
+		if (campaignAdsMap.containsKey("AdTitle")) {
+			updatestr += "`AdTitle` = '" + campaignAdsMap.get("AdTitle") + "',\r\n";
+		}
+		
+		
+		
+		if (campaignAdsMap.containsKey("AdDescription")) {
+			updatestr += "`AdDescription` = '" + campaignAdsMap.get("AdDescription") + "',\r\n";
+		
+		}
+		
+		if (campaignAdsMap.containsKey("AdCount")) {
+			updatestr += "`AdCount` = '" + campaignAdsMap.get("AdCount") + "',\r\n";
+		}
+		
+		if (campaignAdsMap.containsKey("AdDelivered")) {
+			updatestr += "`AdDelivered` = '" + campaignAdsMap.get("AdDelivered") + "',\r\n";
+		
+		}
+		
+		if (campaignAdsMap.containsKey("ImageLocation")) {
+			updatestr += "`ImageLocation` = '" + campaignAdsMap.get("ImageLocation") + "',\r\n";
+		}
+		
+
+		if (campaignAdsMap.containsKey("ImageAltText")) {
+			updatestr += "`ImageAltText` = '" + campaignAdsMap.get("ImageAltText") + "',\r\n";
+		}
+		
+		
+		if (campaignAdsMap.containsKey("StartDate")) {
+			updatestr += "`StartDate` = '" + campaignAdsMap.get("StartDate") + "',\r\n";
+		}
+		if (campaignAdsMap.containsKey("EndDate")) {
+			updatestr += "`EndDate` = '" + campaignAdsMap.get("EndDate") + "',\r\n";
+		}
+		
+		if (campaignAdsMap.containsKey("ReviewStatus")) {
+			updatestr += "`ReviewStatus` = '" + campaignAdsMap.get("ReviewStatus") + "',\r\n";
+		}
+		if (campaignAdsMap.containsKey("PaymentStatus")) {
+			updatestr += "`PaymentStatus` = '" + campaignAdsMap.get("PaymentStatus") + "',\r\n";
+		}
+		
+		updatestr+="LastUpdateddate='"  + date +  "'  ";
+		System.out.println();
+		Query query = session.createNativeQuery(
+				"UPDATE `CampaignAds`\r\n" + "SET\r\n" + updatestr + "WHERE `AdID` = " + AdID + ";");
+		int ret = 0;
+		try {
+		ret = query.executeUpdate();
+		session.getTransaction().commit();
+		System.out.println("updated  table for AdID =  " + AdID);
+	
+	}catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	return ret;
 	}
 	
 }
