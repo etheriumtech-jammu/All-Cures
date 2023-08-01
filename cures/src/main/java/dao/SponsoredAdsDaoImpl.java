e841b7870e915527b37f8eca63e9f2ee0c02fb3d
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-08-01 11:56:09 +0530

    Update SponsoredAdsDaoImpl.java

--- cures/src/main/java/dao/SponsoredAdsDaoImpl.java
+++ cures/src/main/java/dao/SponsoredAdsDaoImpl.java
@@ -14,6 +14,168 @@ import org.hibernate.query.Query;
 import util.HibernateUtil;
 
 public class SponsoredAdsDaoImpl {
+
+	public static Integer InsertCompaniesDetails( HashMap<String, Object> companyMap) {
+
+		Session session = HibernateUtil.buildSessionFactory();
+
+		session.beginTransaction();
+		Date date= Date.valueOf(LocalDate.now());
+		companyMap.put("CreateDate", date);
+		
+		int ret = 0;
+		String insertStr = "INSERT into Companies (" ;
+		
+		String insertStr_values = "(" ;
+		try {
+			// Iterating HashMap through for loop
+			for (Map.Entry<String, Object> companyDetail : companyMap.entrySet()) {
+
+				insertStr += companyDetail.getKey() + ", ";
+
+				if (companyDetail.getValue() instanceof Integer) {
+					insertStr_values += (Integer) (companyDetail.getValue()) + ", ";
+				} else if(companyDetail.getValue() instanceof Date ) {
+					insertStr_values += "'" + (Date) (companyDetail.getValue()) + "' , ";
+				}
+				else
+				{
+					insertStr_values += "'" + (String) (companyDetail.getValue()) + "' , ";
+				}
+				
+
+			}
+
+			insertStr = insertStr.substring(0, insertStr.lastIndexOf(","));
+			insertStr_values = insertStr_values.substring(0, insertStr_values.lastIndexOf(","));
+			String completInsertStr = insertStr + ")" + " values " + insertStr_values + " );";
+			System.out.println(completInsertStr);
+
+			Query query = session.createNativeQuery(completInsertStr);
+			
+			// needs other condition too but unable to find correct column
+			ret = query.executeUpdate();
+			session.getTransaction().commit();
+			System.out.println("Companymap " + companyMap);
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+//		session.getTransaction().commit(); session.close();
+		}
+
+		return ret;
+	}
+
+	public static Integer InsertCampaignDetails( HashMap<String, Object> campaignMap) {
+
+		Session session = HibernateUtil.buildSessionFactory();
+
+		session.beginTransaction();
+		Date date= Date.valueOf(LocalDate.now());
+		campaignMap.put("CreateDate", date);
+		
+		int ret = 0;
+		String insertStr = "INSERT into Campaign (" ;
+		
+		String insertStr_values = "(" ;
+		try {
+			// Iterating HashMap through for loop
+			for (Map.Entry<String, Object> campaignDetail : campaignMap.entrySet()) {
+
+				insertStr += campaignDetail.getKey() + ", ";
+
+				if (campaignDetail.getValue() instanceof Integer) {
+					insertStr_values += (Integer) (campaignDetail.getValue()) + ", ";
+				} else if(campaignDetail.getValue() instanceof Date ) {
+					insertStr_values += "'" + (Date) (campaignDetail.getValue()) + "' , ";
+				}
+				else
+				{
+					insertStr_values += "'" + (String) (campaignDetail.getValue()) + "' , ";
+				}
+				
+
+			}
+
+			insertStr = insertStr.substring(0, insertStr.lastIndexOf(","));
+			insertStr_values = insertStr_values.substring(0, insertStr_values.lastIndexOf(","));
+			String completInsertStr = insertStr + ")" + " values " + insertStr_values + " );";
+			System.out.println(completInsertStr);
+
+			Query query = session.createNativeQuery(completInsertStr);
+			
+			// needs other condition too but unable to find correct column
+			ret = query.executeUpdate();
+			session.getTransaction().commit();
+			System.out.println("Campaignmap " + campaignMap);
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+//		session.getTransaction().commit(); session.close();
+		}
+
+		return ret;
+	}
+
+	public static Integer InsertAdDetails( HashMap<String, Object> AdMap) {
+
+		Session session = HibernateUtil.buildSessionFactory();
+
+		session.beginTransaction();
+		Date date= Date.valueOf(LocalDate.now());
+		AdMap.put("CreateDate", date);
+		
+		int ret = 0;
+		String insertStr = "INSERT into CampaignAds (";
+		
+		String insertStr_values = "(";
+		try {
+			// Iterating HashMap through for loop
+			for (Map.Entry<String, Object> AdDetail : AdMap.entrySet()) {
+
+				insertStr += AdDetail.getKey() + ", ";
+
+				if (AdDetail.getValue() instanceof Integer) {
+					insertStr_values += (Integer) (AdDetail.getValue()) + ", ";
+				} else if(AdDetail.getValue() instanceof Date ) {
+					insertStr_values += "'" + (Date) (AdDetail.getValue()) + "' , ";
+				}
+				else
+				{
+					insertStr_values += "'" + (String) (AdDetail.getValue()) + "' , ";
+				}
+				
+
+			}
+
+			insertStr = insertStr.substring(0, insertStr.lastIndexOf(","));
+			insertStr_values = insertStr_values.substring(0, insertStr_values.lastIndexOf(","));
+			String completInsertStr = insertStr + ")" + " values " + insertStr_values + " );";
+			System.out.println(completInsertStr);
+
+			Query query = session.createNativeQuery(completInsertStr);
+			
+			// needs other condition too but unable to find correct column
+			ret = query.executeUpdate();
+			session.getTransaction().commit();
+			System.out.println("Admap " + AdMap);
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+//		session.getTransaction().commit(); session.close();
+		}
+
+		return ret;
+	}
 	public static List CompaniesDetails() {
 
 		Session session = HibernateUtil.buildSessionFactory();
@@ -56,7 +218,45 @@ public class SponsoredAdsDaoImpl {
 		return hmFinal;
 
 	}
-	
+	public static List CompaniesDetailsByID(int CompanyID) {
+
+		Session session = HibernateUtil.buildSessionFactory();
+
+		Query query = session.createNativeQuery(
+				"Select *  from Companies where CompanyID =" + CompanyID + ";");
+		List<Object[]> results = (List<Object[]>) query.getResultList();
+		System.out.println(results.size());
+		List hmFinal = new ArrayList();
+		for (Object[] objects : results) {
+			LinkedHashMap<String, Object> hm = new LinkedHashMap<>();
+			// add linkedhashmap to preserve the order
+			String CompanyName = (String) objects[1];
+
+			String CompanyWebsite = (String) objects[2];
+
+			String ContactPerson = (String) objects[3];
+
+			String Email = (String) objects[4];
+			String Phone = (String) objects[5];
+			Date CreateDate= (Date) objects[6];
+			Date LastUpdatedDate= (Date) objects[7];
+			
+			
+			hm.put("CompanyName", CompanyName);
+			hm.put("CompanyWebsite", CompanyWebsite);
+			hm.put("ContactPerson", ContactPerson);
+			hm.put("Email", Email);
+			hm.put("Phone", Phone);
+			hm.put("CreateDate", CreateDate);
+			hm.put("LastUpdatedDate", LastUpdatedDate);
+			
+			hmFinal.add(hm);
+
+		}
+
+		return hmFinal;
+
+	}
 	public static List CampaignsDetails() {
 
 		Session session = HibernateUtil.buildSessionFactory();
@@ -97,6 +297,43 @@ public class SponsoredAdsDaoImpl {
 
 		return hmFinal;
 
+	}
+
+	public static List CampaignsDetailsByID(int CampaignID) {
+
+		Session session = HibernateUtil.buildSessionFactory();
+
+		Query query = session.createNativeQuery(
+				"Select *  from Campaign where CampaignID =" + CampaignID + ";");
+		List<Object[]> results = (List<Object[]>) query.getResultList();
+		System.out.println(results.size());
+		List hmFinal = new ArrayList();
+		for (Object[] objects : results) {
+			LinkedHashMap<String, Object> hm = new LinkedHashMap<>();
+			// add linkedhashmap to preserve the order
+			
+			Integer CompanyID = (Integer) objects[1];
+
+			String CampaignName = (String) objects[2];
+
+			Date StartDate = (Date) objects[3];
+
+			Date EndDate = (Date) objects[4];
+			
+			Date CreateDate= (Date) objects[5];
+			Date LastUpdatedDate= (Date) objects[6];
+			
+			hm.put("CompanyID", CompanyID);
+			hm.put("CampaignName", CampaignName);
+			hm.put("StartDate", StartDate);
+			hm.put("EndDate", EndDate);
+			hm.put("CreateDate", CreateDate);
+			hm.put("LastUpdatedDate", LastUpdatedDate);
+			
+			hmFinal.add(hm);
+
+		}
+		return hmFinal;
 	}
 	
 	public static List CampaignsAds() {
@@ -166,6 +403,72 @@ public class SponsoredAdsDaoImpl {
 
 	}
 
+	public static List CampaignsAdsByID(int AdID) {
+
+		Session session = HibernateUtil.buildSessionFactory();
+
+		Query query = session.createNativeQuery(
+				"Select *  from CampaignAds where AdID =" + AdID + ";");
+		List<Object[]> results = (List<Object[]>) query.getResultList();
+		System.out.println(results.size());
+		List hmFinal = new ArrayList();
+		for (Object[] objects : results) {
+			LinkedHashMap<String, Object> hm = new LinkedHashMap<>();
+			// add linkedhashmap to preserve the order
+			
+
+			Integer CampaignID = (Integer) objects[1];
+
+			Integer DiseaseCondition = (Integer) objects[2];
+
+			Integer AdTypeID = (Integer) objects[3];
+
+			String AdTitle = (String) objects[4];
+			
+			String AdDescription= (String) objects[5];
+
+			Integer AdCount = (Integer) objects[6];
+
+			Integer AdDelivered = (Integer) objects[7];
+
+			String ImageLocation = (String) objects[8];
+
+			String ImageAltText= (String) objects[9];
+			Date StartDate = (Date) objects[10];
+
+			Date EndDate = (Date) objects[11];
+				
+			Date CreateDate= (Date) objects[12];
+			Date LastUpdatedDate= (Date) objects[13];
+			
+			Integer ReviewStatus= (Integer) objects[14];
+
+			Integer PaymentStatus= (Integer) objects[15];
+
+			
+			hm.put("CampaignID", CampaignID);
+			hm.put("DiseaseCondition", DiseaseCondition);
+			hm.put("AdTypeID", AdTypeID);
+			hm.put("AdTitle", AdTitle);
+			hm.put("AdDescription", AdDescription);
+			hm.put("AdCount", AdCount);
+			hm.put("AdDelivered", AdDelivered);
+			hm.put("ImageLocation", ImageLocation);
+			hm.put("ImageAltText", ImageAltText);
+			hm.put("StartDate", StartDate);
+			hm.put("EndDate", EndDate);
+			hm.put("CreateDate", CreateDate);
+			hm.put("LastUpdatedDate", LastUpdatedDate);
+			hm.put("ReviewStatus", ReviewStatus);
+			hm.put("PaymentStatus", PaymentStatus);
+
+			hmFinal.add(hm);
+
+		}
+
+		return hmFinal;
+
+	}
 	public static int updateCompanyId(int CompanyID, HashMap companyMap) {
 
 		// creating seession factory object
