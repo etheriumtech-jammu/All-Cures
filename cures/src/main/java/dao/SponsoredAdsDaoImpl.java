44a102676e10eb0d58c0da24056e40436645453e
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-08-01 12:19:27 +0530

    Update SponsoredAdsDaoImpl.java

--- cures/src/main/java/dao/SponsoredAdsDaoImpl.java
+++ cures/src/main/java/dao/SponsoredAdsDaoImpl.java
@@ -1,13 +1,12 @@
 package dao;
 
-
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
-
+import java.util.Map;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
