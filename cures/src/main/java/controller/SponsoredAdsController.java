fb6470a6ac88e1eded08618f847d33d41c975337
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-08-07 15:19:04 +0530

    Update SponsoredAdsController.java

--- cures/src/main/java/controller/SponsoredAdsController.java
+++ cures/src/main/java/controller/SponsoredAdsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-mport org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
