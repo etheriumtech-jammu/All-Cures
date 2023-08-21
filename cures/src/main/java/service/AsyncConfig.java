955169a531b1e030e6944c9574dd09cd112910f1
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-08-21 13:23:27 +0530

    Create AsyncConfig.java

---
+++ cures/src/main/java/service/AsyncConfig.java
@@ -0,0 +1,20 @@
+package service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+	@Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // Set the number of core threads
+        executor.setMaxPoolSize(10); // Set the maximum number of threads
+        executor.setQueueCapacity(25); // Set the queue capacity
+        return executor;
+    }
+}
