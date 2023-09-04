9fc22090c55b3d519dd11422ef9ef838c8d9816c
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-09-04 12:56:23 +0530

    Update DailyTaskScheduler.java

--- cures/src/main/java/util/DailyTaskScheduler.java
+++ cures/src/main/java/util/DailyTaskScheduler.java
@@ -74,9 +74,9 @@ public class DailyTaskScheduler {
     	Session session = HibernateUtil.buildSessionFactory();
     	  int adCountPerDay=0;
 		Query query = session.createNativeQuery(
-				"SELECT AdID,ImageLocation, StartDate, EndDate,AdCount,(AdCount-AdDelivered), AdTypeName\r\n"
-				+ "FROM CampaignAds c join AdsTypes t\r\n"
-				+ "WHERE CURDATE() BETWEEN StartDate AND EndDate and ReviewStatus=1 and c.AdTypeID=t.AdTypeID and DiseaseCondition=0 ");
+				"SELECT AdID,ImageLocation, StartDate, EndDate,AdCount,(AdCount-AdDelivered), SlotName\r\n"
+				+ "FROM CampaignAds c join AdsSlotTypes t\r\n"
+				+ "WHERE CURDATE() BETWEEN StartDate AND EndDate and ReviewStatus=1 and c.AdTypeID=t.SlotID and DiseaseCondition=0 ");
 		List<Object[]> results = (List<Object[]>) query.getResultList();
 
 
