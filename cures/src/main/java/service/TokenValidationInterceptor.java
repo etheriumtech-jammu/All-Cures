401176520e7c8e6ca6c1584f43599b08b7c09d2f
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-10-04 13:22:29 +0530

    Create TokenValidationInterceptor.java

---
+++ cures/src/main/java/service/TokenValidationInterceptor.java
@@ -0,0 +1,73 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class TokenValidationInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(TokenValidationInterceptor.class);
+  
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("Authorization");
+        logger.info("TokenValidationInterceptor: Request received with token: {}", token);
+        try {
+            if (!(token == null )) {
+            	  int result=TokenValidator.isValidToken(token);
+            	if(result==0)
+            	{
+            		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or missing token.");
+            		return false;
+            	}
+            	else if(result==2)
+            	{
+            		response.sendError(HttpServletResponse.SC_FORBIDDEN, "You have crossed the Maximum Attempts.");	
+            	}	
+            }
+            else
+            {
+            	response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or missing token.");
+            	return false;
+            }
+        } catch (Exception e) {
+            logger.error("Error during token validation: {}", e.getMessage());
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred during token validation.");
+            return false;
+        } 
+        return true;
+    }
+    
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        // This method is called after the handler method is invoked (after the API call).
+        // You can perform additional actions here if needed.
+     }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // This method is called after the response has been sent to the client.
+        // You can perform additional actions here if needed.
+    }
+}
+
+/* <mvc:interceptors>
+<mvc:interceptor>
+<!-- First mapping path -->
+<mvc:mapping path="/city/**"/>
+<bean class="service.TokenValidationInterceptor" />
+</mvc:interceptor>
+<mvc:interceptor>
+<!-- Second mapping path -->
+<mvc:mapping path="/cures/get/**"/>
+<bean class="service.AnotherInterceptor" />
+</mvc:interceptor>
+</mvc:interceptors>
+*/
