1a7ccd89840c3f38901997da3892a87ca16a2d65
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-10-06 12:36:18 +0530

    Update TokenValidator.java

--- cures/src/main/java/service/TokenValidator.java
+++ cures/src/main/java/service/TokenValidator.java
@@ -5,7 +5,7 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.List;
-
+import org.springframework.scheduling.annotation.Async;
 import javax.persistence.NoResultException;
 
 import org.hibernate.Session;
@@ -14,126 +14,88 @@ import org.hibernate.query.Query;
 import util.HibernateUtil;
 
 public class TokenValidator {
+    private static final String STATIC_TOKEN = "All-Cures";
 
     public static int isValidToken(String token, String url) {
-    	
-    	 if (token.startsWith("Bearer ")) {
-  	        token = token.substring("Bearer ".length());
-  	    }
-    	 
-    	 System.out.println("token" + token);
- 		Session session = HibernateUtil.buildSessionFactory();
- 		int res=0;
- 		Date current_Date= generating_Current_Date();
- 		 Query query = session.createNativeQuery("SELECT\r\n"
- 		 		+ "    at.TokenID,\r\n"
- 		 		+ "    at.Token,\r\n"
- 		 		+ "    at.Status,\r\n"
- 		 		+ "    at.Max_Allowed,\r\n"
- 		 		+ "    aa.Total_Count,\r\n"
- 		 		+ "    aa.LastUpdateDate\r\n"
- 		 		+ "FROM\r\n"
- 		 		+ "    API_Tokens at\r\n"
- 		 		+ "JOIN\r\n"
- 		 		+ "    APITokenAnalytics aa\r\n"
- 		 		+ "ON\r\n"
- 		 		+ "    at.TokenID = aa.TokenID\r\n"
- 		 		+ "where Token =   '"+token+"' and aa.API='"+url+ "' \r\n"
- 		 		+ ";");
- 		 try {
- 			 List<Object[]> results =(List<Object[]>)  query.getResultList();
- 			 
- 			 if(results.size()>0) {
- 				 
- 				 for(Object[] list:results) {
- 					 
- 					Integer TokenID= (Integer) list[0];
- 					String Token=(String) list[1];
- 					Integer Status=(Integer) list[2];
- 					Integer Max_Allowed=(Integer) list[3]==null?0:(Integer) list[3];
- 					Integer Total_Count=(Integer) list[4]==null?0:(Integer) list[4];
- 					Date LastUpdateDate=(Date) list[5];
- 					 
- 					// 1 for update
- 					// 2 for increment
- 					
- 					if(token.equals(Token) && Status == 1) {
- 						if(Max_Allowed==Total_Count &&  false == current_Date.equals(LastUpdateDate)) {
- 							res=update_Total_Count(url,TokenID,Total_Count, 1);
- 							
- 						}
- 						if(Max_Allowed==Total_Count && true == current_Date.equals(LastUpdateDate)) {
- 							res=2;
- 						}
- 						if(Max_Allowed == 0) {
- 							res=1;
- 						}
- 						if(Max_Allowed > Total_Count) {
- 							res = update_Total_Count(url,TokenID,Total_Count, 2);
- 							
- 						}
- 					}
- 					
- 				 }
- 			 }
- 			 
- 			  
- 		 } catch (NoResultException e) {
- 				System.out.println("No Entry");
+        if (token.startsWith("Bearer ")) {
+            token = token.substring("Bearer ".length());
+        }
 
- 			}
- 		 System.out.println("res"+res);
- 		 return res;
+        Session session = HibernateUtil.buildSessionFactory();
+        int res = 0;
+
+        Date current_Date = generatingCurrentDate();
+        @SuppressWarnings("unchecked")
+		Query<Object[]> query = session.createNativeQuery(
+                "SELECT at.TokenID, at.Token, at.Status, at.Max_Allowed, aa.Total_Count, aa.LastUpdateDate FROM API_Tokens at JOIN APITokenAnalytics aa ON at.TokenID = aa.TokenID WHERE at.Token = :token AND aa.API = :url")
+                .setParameter("token", token)
+                .setParameter("url", url);
+
+        try {
+            List<Object[]> results = query.getResultList();
+
+            for (Object[] list : results) {
+                Integer TokenID = (Integer) list[0];
+                String Token = (String) list[1];
+                Integer Status = (Integer) list[2];
+                Integer Max_Allowed = (Integer) (list[3] == null ? 0 : list[3]);
+                Integer Total_Count = (Integer) (list[4] == null ? 0 : list[4]);
+                Date LastUpdateDate = (Date) list[5];
+
+                if (token.equals(Token) && Status == 1) {
+                    if (Max_Allowed == Total_Count && false == current_Date.equals(LastUpdateDate)) {
+                        res = updateTotalCount(url, TokenID, Total_Count, 1, session);
+                    } else if (Max_Allowed == Total_Count && true == current_Date.equals(LastUpdateDate)) {
+                        res = 2;
+                    } else if (Max_Allowed == 0) {
+                        res = 1;
+                    } else if (Max_Allowed > Total_Count) {
+                        res = updateTotalCount(url, TokenID, Total_Count, 2, session);
+                    }
+                }
+            }
+        } catch (NoResultException e) {
+            System.out.println("No Entry");
+        } finally {
+   //         session.close(); // Close the session when done
+        }
+
+        return res;
     }
-    
-    public static int update_Total_Count(String url,Integer tokenID,Integer Total_count, int toDo) {
-		Session session = HibernateUtil.buildSessionFactory();
-	      session.beginTransaction();
-	      int ret=0;  
-	      String str = "";
-	      if(Total_count ==0) {
-	    	  str="UPDATE APITokenAnalytics\r\n"
-	    	  		+ "SET Total_Count = 1\r\n"
-	    	  		+ "where TokenID="+tokenID+" and API = '"+url+"'\r\n"
-	    	  		+ ";";
-	    	  toDo=0;
-	      }
-	      if(toDo ==1) {
-	    	  str="UPDATE APITokenAnalytics\r\n"
-	    	  		+ "SET Total_Count = 1\r\n"
-	    	  		+ "where TokenID="+tokenID+" and API = '"+url+"'\r\n"
-	    	  		+ ";";
-	      }
-	      if(toDo==2) {
-	    	  str="UPDATE APITokenAnalytics\r\n"
-	    	  		+ "SET Total_Count = Total_Count+1\r\n"
-	    	  		+ "where TokenID="+tokenID+" and API = '"+url+"'\r\n"
-	    	  		+ ";";
-	      }
-	       
-	      try {
-	    	  Query query = session.createNativeQuery(str);
-	      ret = query.executeUpdate();
-	      
-	      session.getTransaction().commit();
-	       
-	     
-//	      System.out.println(ret);
-	      }catch (Exception e) {
-				e.printStackTrace();
-				session.getTransaction().rollback();
-			}
-		
-		
-		return ret;
-	}
-	
-	
-	public static Date generating_Current_Date() {
-		LocalDate currentDate = LocalDate.now();
+
+    @Async
+    public static int updateTotalCount(String url, Integer tokenID, Integer totalCount, int toDo, Session session) {
+        session.beginTransaction();
+        int ret = 0;
+        String str = "";
+
+        if (totalCount == 0) {
+            str = "UPDATE APITokenAnalytics SET Total_Count = 1 WHERE TokenID = :tokenID AND API = :url";
+            toDo = 0;
+        } else if (toDo == 1) {
+            str = "UPDATE APITokenAnalytics SET Total_Count = 1 WHERE TokenID = :tokenID AND API = :url";
+        } else if (toDo == 2) {
+            str = "UPDATE APITokenAnalytics SET Total_Count = Total_Count + 1 WHERE TokenID = :tokenID AND API = :url";
+        }
+
+        try {
+            Query<?> query = session.createNativeQuery(str)
+                    .setParameter("tokenID", tokenID)
+                    .setParameter("url", url);
+            ret = query.executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+
+        return ret;
+    }
+
+    public static Date generatingCurrentDate() {
+        LocalDate currentDate = LocalDate.now();
         ZoneId zoneId = ZoneId.systemDefault();
         ZonedDateTime zonedDateTime = currentDate.atStartOfDay(zoneId);
-        Date nowDate = Date.from(zonedDateTime.toInstant());
-		return nowDate;
-	}
+        return Date.from(zonedDateTime.toInstant());
+    }
 }
