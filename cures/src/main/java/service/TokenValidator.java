0e1c32fa0f60ee63700e2a22bb4f96fec5366331
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-10-09 16:17:43 +0530

    Update TokenValidator.java

--- cures/src/main/java/service/TokenValidator.java
+++ cures/src/main/java/service/TokenValidator.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.Async;
 import javax.persistence.NoResultException;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.annotation.Isolation;
-
+import java.util.concurrent.atomic.AtomicInteger;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import util.HibernateUtil;
@@ -42,13 +42,14 @@ public class TokenValidator {
 				String Token = (String) list[1];
 				Integer Status = (Integer) list[2];
 				Integer Max_Allowed = (Integer) (list[3] == null ? 0 : list[3]);
-				Integer Total_Count = (Integer) (list[4] == null ? 0 : list[4]);
+				// Integer Total_Count = (Integer) (list[4] == null ? 0 : list[4]);
+				AtomicInteger Total_Count = new AtomicInteger((Integer) (list[4] == null ? 0 : list[4]));
 				Date LastUpdateDate = (Date) list[5];
 
 				if (token.equals(Token) && Status == 1) {
 					
 					
-					if (Max_Allowed == Total_Count && false == current_Date.equals(LastUpdateDate)) {
+					if (Max_Allowed == Total_Count.get() && false == current_Date.equals(LastUpdateDate)) {
 						session.beginTransaction();
 						String str = "UPDATE APITokenAnalytics SET Total_Count = 1 "
 								+ "WHERE TokenID = :tokenID AND API = :url";
@@ -63,11 +64,11 @@ public class TokenValidator {
 							session.getTransaction().rollback();
 						}
 						}
-					 else if (Max_Allowed == Total_Count && true == current_Date.equals(LastUpdateDate)) {
+					 else if (Max_Allowed == Total_Count.get() && true == current_Date.equals(LastUpdateDate)) {
 						res = 2;
 					} else if (Max_Allowed == 0) {
 						res = 1;
-					} else if (Max_Allowed > Total_Count) {
+					} else if (Max_Allowed > Total_Count.get()) {
 						session.beginTransaction();
 						String str = "UPDATE APITokenAnalytics SET Total_Count = Total_Count + 1 "
 								+ "WHERE TokenID = :tokenID AND API = :url";
