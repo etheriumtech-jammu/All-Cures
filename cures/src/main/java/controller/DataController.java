b992fc038fb086f44dde6bdc59a7c06255575724
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-10-23 15:04:55 +0530

    Update DataController.java
    image api

--- cures/src/main/java/controller/DataController.java
+++ cures/src/main/java/controller/DataController.java
@@ -118,4 +118,10 @@ public class DataController {
 		
 	}
 
+	@RequestMapping(value = "/doctor/image", produces = "application/json", method = RequestMethod.GET)
+	public List doctor_image() throws IOException {
+		return DataDaoImpl.doctor_image();
+		
+	}
+
 }
