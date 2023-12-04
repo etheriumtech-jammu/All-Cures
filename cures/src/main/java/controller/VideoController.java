ad7a73cb316e2de231b3ec574881578007e8af51
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-12-04 16:36:35 +0530

    Update VideoController.java

--- cures/src/main/java/controller/VideoController.java
+++ cures/src/main/java/controller/VideoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import dao.VideoDaoImpl;
+//import dao.VideoDaoImpl;
 
 @RestController
 @RequestMapping(path = "/video")
