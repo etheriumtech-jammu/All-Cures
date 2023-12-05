5fcf35c16c0844a8c8e99d548a6604439023dbe8
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-12-05 10:49:18 +0530

    Create VideoController.java

---
+++ cures/src/main/java/controller/VideoController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class VideoController {
+@RequestMapping(value = "/all", produces = "application/json", method = RequestMethod.GET)
+	
+	public @ResponseBody int getCityDetails() {
+		
+		return 1;
+
+	}
+}
