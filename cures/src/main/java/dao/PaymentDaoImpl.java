577cb2a109d7cb1c338cb96fbc90ed77a40f07b0
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-12-06 15:15:35 +0530

    Update PaymentDaoImpl.java

--- cures/src/main/java/dao/PaymentDaoImpl.java
+++ cures/src/main/java/dao/PaymentDaoImpl.java
@@ -11,7 +11,7 @@ import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import model.ServicePayment;
-
+import model.ServicePaymentMethod;
 import util.Constant;
 import util.HibernateUtil;
 
@@ -184,7 +184,176 @@ public class PaymentDaoImpl {
 
 		    return ServicePaymentList;
 		}
+
+	// Method that sets the payment method for a particular service  
+	  public static Integer InsertPaymentMethod( HashMap<String, Object> PaymentMethodMap) { 
+		  Session session = HibernateUtil.buildSessionFactory();
+		  ServicePaymentMethod method= new ServicePaymentMethod();
+		  
+	        try {
+	            // Assuming your HashMap has keys matching the property names in Service
+	            // Adjust these names based on your actual Service class
+	        	Transaction tx = session.beginTransaction();
+	        	method.setServiceID((Integer) PaymentMethodMap.get("ServiceID"));
+	        	method.setServicePaymentMasterID((Integer) PaymentMethodMap.get("ServicePaymentMasterID"));
+	        	method.setCreatedBy((Integer) PaymentMethodMap.get("CreatedBy"));
+	        	method.setStatus((Integer) PaymentMethodMap.get("Status"));
+	            session.save(method);
+	            tx.commit();
+	            // Return 1 if insertion is successful
+	            return 1;
+	        } catch (Exception e) {
+	            e.printStackTrace(); // Log the exception or handle it appropriately
+	            session.getTransaction().rollback();
+	            // Return 0 if insertion fails
+	            return 0;
+	        }
+	    }
+	
+	  public static List<ServicePaymentMethod> getPaymentMethods() {
+		    Session session = HibernateUtil.buildSessionFactory();
+		    Query query1 = session.createNativeQuery("SELECT * FROM servicepaymentmethod;");
+		    List<ServicePaymentMethod> PaymentMethodList = new ArrayList<>();
+		    
+		    List<Object[]> resultList = query1.getResultList();
+		    Constant.log("Executed Query and Got: " + resultList.size() + " Schedule Lists back", 1);
+
+		    for (Object[] obj : resultList) {
+		    	ServicePaymentMethod method = new ServicePaymentMethod();
+		    	
+		    	method.setServicePaymentMethodID(obj[0] != null ? (Integer) obj[0] : 0);
+		    	method.setServicePaymentMasterID(obj[1] != null ? (Integer) obj[1] : 0);
+		    	method.setServiceID(obj[2] != null ? (Integer) obj[2] : 0);
+		    	
+		    	method.setCreatedBy(obj[3] != null ? (Integer) obj[3] : 0);
+		    	method.setCreatedDate((Timestamp) (obj[4] != null ? obj[4] : null));
+		    	method.setLastUpdatedDate((Timestamp) (obj[5] != null ? obj[5] : null));
+		    	method.setStatus(obj[6] != null ? (Integer) obj[6] : 0);
+		    	method.setUpdatedBy(obj[7] != null ? (Integer) obj[7] : 0);
+		    	
+		    	
+		    	PaymentMethodList.add(method);
+		    }
+
+		    return PaymentMethodList;
+		}
 	  
+	// Method to update a ServiceContract based on ContractID and provided ContractMap
+	    
+		  public static int updatePaymentMethod(Integer ServicePaymentMethodID, HashMap PaymentMethodMap) {
+			// String to store the update clauses
+		        String updatestr = "";
+
+		        // Construct the update clauses based on the keys in ContractMap
+		       
+			 
+				if (PaymentMethodMap.containsKey("ServiceID")) {
+				    updatestr += "ServiceID = '" + PaymentMethodMap.get("ServiceID") + "',\r\n";
+				}
+			  if (PaymentMethodMap.containsKey("ServicePaymentMasterID")) {
+					updatestr +=" ServicePaymentMasterID = " + PaymentMethodMap.get("ServicePaymentMasterID") + ",\r\n";
+				}
+			  
+			  if (PaymentMethodMap.containsKey("UpdatedBy")) {
+					updatestr += " UpdatedBy = " + PaymentMethodMap.get("UpdatedBy") + ",\r\n";
+				}
+			 
+			  if (PaymentMethodMap.containsKey("Status")) {
+					updatestr += " Status = " + PaymentMethodMap.get("Status") + ",\r\n";
+				}
+			// Remove trailing comma from the update string
+		        updatestr = updatestr.replaceAll(",$", "");
+
+		        // Create a Hibernate session
+		        Session session = HibernateUtil.buildSessionFactory();
+
+		        // Begin a transaction
+		        session.beginTransaction();
+
+		        // Create a native SQL query to update servicecontractdetails table
+		        Query query = session.createNativeQuery("UPDATE servicepaymentmethod SET " + updatestr + " WHERE ServicePaymentMethodID = " + ServicePaymentMethodID + ";");
+
+		        int ret = 0;  // Variable to store the result of the update
+
+		        try {
+		            // Execute the update query
+		            ret = query.executeUpdate();
+
+		            // Log success message
+		            Constant.log(" Updated ServicePaymentMethod table for ServicePaymentMethodID = " + ServicePaymentMethodID, 1);
+
+		            // Commit the transaction
+		            session.getTransaction().commit();
+		        } catch (Exception ex) {
+		            // Rollback the transaction if an exception occurs
+		            session.getTransaction().rollback();
+		        } finally {
+		            // Uncomment the line below if you want to close the session here
+		            // session.close();
+		        }
+
+		        // Return the result of the update
+		        return ret;
+		    }
+
+		  public static int deletePaymentMethod(int ServicePaymentMethodID) {
+				
+				Session session = HibernateUtil.buildSessionFactory();
+
+				// creating session object
+				//Session session = factory;
+				// creating transaction object
+				session.beginTransaction();
+				
+				 String updatestr = " status = 0  ";
+				 
+				 System.out.println(updatestr);
+					Query query = session.createNativeQuery(
+							"UPDATE `ServicePaymentMethod`\r\n" + "SET\r\n" + updatestr + "WHERE `ServicePaymentMethodID` = " + ServicePaymentMethodID + ";");
+					int ret = 0;
+					try {
+					ret = query.executeUpdate();
+					session.getTransaction().commit();
+					System.out.println("deleted entry for ServicePaymentMethodID =  " + ServicePaymentMethodID);
+				
+				}catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				return ret;
+				 	 
+			}
+		  
+		  public static List<ServicePaymentMethod> getPaymentMethod(int ServicePaymentMethodID) {
+			    Session session = HibernateUtil.buildSessionFactory();
+			    Query query1 = session.createNativeQuery("SELECT * FROM servicepaymentmethod where ServicePaymentMethodID =" + ServicePaymentMethodID + ";");
+			    List<ServicePaymentMethod> PaymentMethodList = new ArrayList<>();
+			    
+			    List<Object[]> resultList = query1.getResultList();
+			    Constant.log("Executed Query and Got: " + resultList.size() + " Schedule Lists back", 1);
+
+			    for (Object[] obj : resultList) {
+			    	ServicePaymentMethod method = new ServicePaymentMethod();
+			    	
+			    	method.setServicePaymentMethodID(obj[0] != null ? (Integer) obj[0] : 0);
+			    	method.setServicePaymentMasterID(obj[1] != null ? (Integer) obj[1] : 0);
+			    	method.setServiceID(obj[2] != null ? (Integer) obj[2] : 0);
+			    	
+			    	method.setCreatedBy(obj[3] != null ? (Integer) obj[3] : 0);
+			    	method.setCreatedDate((Timestamp) (obj[4] != null ? obj[4] : null));
+			    	method.setLastUpdatedDate((Timestamp) (obj[5] != null ? obj[5] : null));
+			    	method.setStatus(obj[6] != null ? (Integer) obj[6] : 0);
+			    	method.setUpdatedBy(obj[7] != null ? (Integer) obj[7] : 0);
+			    	
+			    	
+			    	PaymentMethodList.add(method);
+			    }
+
+			    return PaymentMethodList;
+			}
+		  
+		  
 
 	
 }
