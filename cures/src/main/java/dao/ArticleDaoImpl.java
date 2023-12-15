4ab06791e3ec6826ccacfdba551e6750479f97f6
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-12-15 10:50:48 +0530

    Update ArticleDaoImpl.java

--- cures/src/main/java/dao/ArticleDaoImpl.java
+++ cures/src/main/java/dao/ArticleDaoImpl.java
@@ -13,7 +13,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-
+import javax.persistence.StoredProcedureQuery;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
