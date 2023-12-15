a8d16f9a95646e608dfbf53e09bd1b452af80400
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2023-12-15 10:36:29 +0530

    Update ArticleController.java
    add api of ranked articles

--- cures/src/main/java/controller/ArticleController.java
+++ cures/src/main/java/controller/ArticleController.java
@@ -112,6 +112,11 @@ public class ArticleController {
 		return articleDaoImpl.getArticlesListAllKeysFeatured(limit,offset,search,order);
 	}
 	
+	@RequestMapping(value = "/allkvranked", produces = "application/json", method = RequestMethod.GET)
+	public @ResponseBody List listArticlesAllKeysRanked(@RequestParam(required = false) Integer limit,@RequestParam(required = false) Integer offset,@RequestParam(required = false) String search,@RequestParam(required = false) String order) {
+		return articleDaoImpl.getArticlesListAllKeysRanked(limit,offset,search,order);
+	}
+	
 	@RequestMapping(value = "/authallkv/reg_type/{reg_type}/reg_doc_pat_id/{reg_doc_pat_id}", produces = "application/json", method = RequestMethod.GET)
 	public @ResponseBody List listArticlesAllKeysByRegTypeAndID(@RequestParam(required = false) Integer limit, @PathVariable int reg_type, @PathVariable int reg_doc_pat_id, @RequestParam(required = false) Integer offset) {
 		return articleDaoImpl.getArticlesListAllKeysbyAuthIdandregType(reg_type, reg_doc_pat_id, limit, offset);
