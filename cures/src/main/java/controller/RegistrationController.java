96256ff5fe6342e2c5080c89e2732a38cdfb4663
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-02-07 16:08:15 +0530

    Update RegistrationController.java

--- cures/src/main/java/controller/RegistrationController.java
+++ cures/src/main/java/controller/RegistrationController.java
@@ -1,44 +1,27 @@
 package controller;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import dao.CityDaoImpl;
-import dao.RegistrationDaoImpl;
 import dao.RegistrationDaoImpl_New;
-import dao.VideoDaoImpl;
-import model.Registration;
-import util.Constant;
-import util.CookieManager;
-import util.EnDeCryptor;
-
 @RestController
 @RequestMapping(path = "/registration")
 public class RegistrationController {
 
+	//To register a new User
 	@RequestMapping(value = "/add/new", produces = "application/json", method = RequestMethod.POST)
-	public @ResponseBody String RegisterUser(@RequestBody HashMap RegisterMap,HttpServletRequest request,HttpServletResponse response ) throws Exception {
-
-	return	RegistrationDaoImpl_New.RegisterUser(RegisterMap,request,response);
-	  
+	public @ResponseBody void RegisterUser(@RequestBody HashMap RegisterMap,HttpServletRequest request,HttpServletResponse response ) throws Exception {
+	String res=	RegistrationDaoImpl_New.RegisterUser(RegisterMap,request,response);
+	PrintWriter out = response.getWriter();
+   	 out.write(res);
+   	 out.flush();
 	}
-	
+    
 }
 	
