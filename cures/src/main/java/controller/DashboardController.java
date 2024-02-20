bc2000b8264024049555365d0a1c25382d02d23e
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-02-20 15:03:01 +0530

    Update DashboardController.java

--- cures/src/main/java/controller/DashboardController.java
+++ cures/src/main/java/controller/DashboardController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+import org.hibernate.Session;
 import dao.ArticleDaoImpl;
 import model.Article;
 import model.Article_dc_name;
