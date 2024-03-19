b8956b1980c04fbc9026c023d2b39f992091291c
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-03-19 12:10:35 +0530

    Update SearchDaoImpl.java

--- cures/src/main/java/dao/SearchDaoImpl.java
+++ cures/src/main/java/dao/SearchDaoImpl.java
@@ -172,7 +172,7 @@ public class SearchDaoImpl {
 					doc.getAddress1(), doc.getCity(), doc.getState(), doc.getCountry(),
 					doc.getOverAllRating(), doc.getPrefix(),
 					doc.getFirstName(), doc.getMiddleName(), doc.getLastName(), doc.getFullName(),
-					doc.getEmail(), doc.getAbout(),doc.getImgLoc()));
+					doc.getEmail(), doc.getAbout(),doc.getImgLoc(),doc.getMedicineTypeID()));
 			// System.out.println("id: " + doctorid + "; for: " + docname_first);
 
 		}
@@ -329,7 +329,7 @@ public class SearchDaoImpl {
 					doc.getAddress1(), doc.getCity(), doc.getState(), doc.getCountry(),
 					doc.getOverAllRating(), doc.getPrefix(),
 					doc.getFirstName(), doc.getMiddleName(), doc.getLastName(), doc.getFullName(),
-					doc.getEmail(),   doc.getAbout(),doc.getImgLoc()));
+					doc.getEmail(),   doc.getAbout(),doc.getImgLoc(),doc.getMedicineTypeID()));
 			// System.out.println("id: " + doctorid + "; for: " + docname_first);
 
 		}
@@ -461,7 +461,7 @@ public class SearchDaoImpl {
 					doc.getAddress1(), doc.getCity(), doc.getState(), doc.getCountry(),
 					doc.getOverAllRating(), doc.getPrefix(),
 					doc.getFirstName(), doc.getMiddleName(), doc.getLastName(), doc.getFullName(),
-					doc.getEmail(), doc.getAbout(),doc.getImgLoc()));
+					doc.getEmail(), doc.getAbout(),doc.getImgLoc(),doc.getMedicineTypeID()));
 			// System.out.println("id: " + doctorid + "; for: " + docname_first);
 
 		}
