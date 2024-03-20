fa7610960566391c5711a956f448bba833bba260
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-03-20 13:36:39 +0530

    Update SearchDaoImpl.java

--- cures/src/main/java/dao/SearchDaoImpl.java
+++ cures/src/main/java/dao/SearchDaoImpl.java
@@ -198,33 +198,36 @@ public class SearchDaoImpl {
 		// MapSolrParams queryParams = new MapSolrParams(queryParamMap);
 //		System.out.println("Lat" + lat);
 //		System.out.println("Lon" + lon);
-		String lat="74.8570";
-		String lon="74.8570";
-		String docloc = lat + "," + lon;
+		
 		SolrQuery query = new SolrQuery();
 		String[] dname = docdetails.split(" ");
 		System.out.println("length" + dname.length);
 		StringBuilder queryStringBuilder = new StringBuilder();
 		if (dname[0].contains("Dr") || dname[0].contains("Dr.")) {
-		    
 		    // Append the field name and the first name
-		    queryStringBuilder.append("docname_first:").append(dname[1]);
-		    
-		    // Append OR clauses for middle and last names if they exist
-		    for (int i = 2; i < dname.length; i++) {
-		        queryStringBuilder.append(" OR ");
-		        if (i == dname.length - 1) {
-		            // Last name
-		            queryStringBuilder.append("docname_last:").append(dname[i]);
-		        } else {
-		            // Middle name
-		            queryStringBuilder.append("docname_middle:").append(dname[i]);
-		        }
-		    }
-		    
-		    // Add the constructed query to the Solr query object
+	    queryStringBuilder.append("docname_first:").append(dname[1]);
+    
+	    // Flag to check if any middle or last names have been added
+	    boolean addedMiddleOrLastName = false;
+    
+	    // Append OR clauses for middle and last names if they exist
+	    for (int i = 2; i < dname.length; i++) {
+	        if (!dname[i].trim().isEmpty()) {
+	            if (addedMiddleOrLastName) {
+                queryStringBuilder.append(" OR ");
+	            }
+	            if (i == dname.length - 1) {
+                // Last name
+                queryStringBuilder.append("docname_last:").append(dname[i]);
+	            } else {
+                // Middle name
+ 	               queryStringBuilder.append("docname_middle:").append(dname[i]);
+	            }
+	            addedMiddleOrLastName = true;
+	        }
+	      // Add the constructed query to the Solr query object
 		    query.add("q", queryStringBuilder.toString());
-		} else {
+		} }else {
 		    // Handle cases where the name doesn't contain "Dr" or "Dr."
 		    query.add("q", Constant.NAME + ":" + docdetails + Constant.OR + Constant.PRIMARY_SPL + ":" + docdetails + Constant.OR + Constant.SUB_SPLS + ":" + docdetails);
 		}
@@ -233,7 +236,7 @@ public class SearchDaoImpl {
 //		query.add("pt", docloc);
 //		query.add("sfield", "docloc1");
 //		query.add("d", "50");
-		
+
 		System.out.println("Query: " + queryStringBuilder.toString());
 		QueryResponse response = null;
 		try {
@@ -344,6 +347,7 @@ public class SearchDaoImpl {
 
 		return docarr;
 	}
+
 
 	public static List<Doctor> searchByBoth(String docdetails, String cityname) {
 		SolrClient client = SolrUtil.buildSolrFactory();
