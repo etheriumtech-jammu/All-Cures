694d4ffb1940aa7056abf412fe75ccd73614872c
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-03-21 15:52:55 +0530

    Create PaymentGatewayDaoImpl.java

---
+++ cures/src/main/java/dao/PaymentGatewayDaoImpl.java
@@ -0,0 +1,54 @@
+package dao;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import model.AvailabilitySchedule;
+import util.AesCryptUtil;
+import util.HibernateUtil;
+
+public class PaymentGatewayDaoImpl {
+
+	
+	public static String SetPayment(HashMap<String, Object> AppointmentMap) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		String orderId="";
+		Integer i=1;
+		Query query = session.createNativeQuery("SELECT MAX(order_id) FROM orders");
+		Integer highestOrderId = (Integer) query.uniqueResult();
+		if(highestOrderId==null)
+		{
+			orderId=i.toString();
+		}else {
+			i=highestOrderId + 1;
+			orderId=i.toString();
+		}
+		
+		
+		String currency=(String) AppointmentMap.get("currency");
+		String workingKey="80923CFC322F5875BA18A25A84B3F05B";
+		String amount=(String) AppointmentMap.get("amount");
+		String redirect_url="https://uat.all-cures.com:444/cures/ccavResponsehandler.jsp";
+		String cancel_url="https://uat.all-cures.com:444/cures/Error.jsp";
+		long currentTimeMillis = new Date().getTime();
+        int ccaRequesttid = (int) currentTimeMillis;
+         String merchant_id="3119096";
+         
+         String ccaRequest = "ccaRequesttid=" + ccaRequesttid +
+        	        "&merchant_id=" + merchant_id +
+        	        "&order_id=" + orderId +
+        	        "&currency=" + currency +
+        	        "&amount=" + amount +
+        	        "&redirect_url=" + redirect_url +
+        	        "&cancel_url=" + cancel_url +
+        	        "&language=EN" ;
+         AesCryptUtil aesUtil=new AesCryptUtil(workingKey);
+    	 String encRequest = aesUtil.encrypt(ccaRequest);
+         System.out.println(encRequest);
+		return encRequest;
+		
+	}
+}
