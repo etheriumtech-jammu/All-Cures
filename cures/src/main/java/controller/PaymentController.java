7e91fb8f1541e338e402d827e31c1419c0b1a19d
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-03-29 14:06:15 +0530

    Update PaymentController.java

--- cures/src/main/java/controller/PaymentController.java
+++ cures/src/main/java/controller/PaymentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 import dao.PaymentDaoImpl;
 import dao.PaymentGatewayDaoImpl;
