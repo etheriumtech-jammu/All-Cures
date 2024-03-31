599a8b6cf074d1f46344b55dc310955571e5744c
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-03-31 16:13:45 +0530

    Update TokenValidationInterceptor.java

--- cures/src/main/java/service/TokenValidationInterceptor.java
+++ cures/src/main/java/service/TokenValidationInterceptor.java
@@ -18,11 +18,11 @@ public class TokenValidationInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String userAgent = request.getHeader("User-Agent");
-    // Check if the user agent indicates a mobile device
-   if (userAgent != null && userAgent.toLowerCase().contains("mobile")) {
-        // If it's a mobile device, skip token validation
-        return true;
-    }
+ // Check if the user agent indicates a mobile device or MyApp
+if (userAgent != null && (userAgent.toLowerCase().contains("mobile") || userAgent.contains("MyApp"))) {
+    // If it's a mobile device or MyApp, skip token validation
+    return true;
+}
         
         String token = request.getHeader("Authorization");
         String url= request.getServletPath().toString();
