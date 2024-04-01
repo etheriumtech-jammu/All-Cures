781ac75eb16c103ff04a39aa9475733b0a6d82df
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-04-01 15:45:47 +0530

    Update ContentActionController.java

--- cures/src/main/java/controller/ContentActionController.java
+++ cures/src/main/java/controller/ContentActionController.java
@@ -309,8 +309,6 @@ public class ContentActionController extends HttpServlet {
 		}else if (finalAction.equals("updateArticle")) {
 			Constant.log("Updating Article:"+article_id, 1);
 			boolean artStatus = updateArticle(request, response, Integer.parseInt(article_id), regi.getRegistration_id().intValue());
-			
-			System.out.println(artStatus);
 			if(!ajaxCall){
 				//Set the article object in the session or servlet Context
 				Constant.log("Sending to article.jsp:"+artJson, 1);
