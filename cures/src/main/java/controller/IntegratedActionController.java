f46743cb79c2cbd8dca314d24a32506ba88072ca
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-04-16 13:06:25 +0530

    Update IntegratedActionController.java

--- cures/src/main/java/controller/IntegratedActionController.java
+++ cures/src/main/java/controller/IntegratedActionController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.json.JSONObject;
-
+import org.xerial.snappy.SnappyOutputStream;
 import com.google.gson.Gson;
 
 import dao.DoctorsDaoImpl;
