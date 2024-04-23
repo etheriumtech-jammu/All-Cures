c0efe21bb87773d18d4d400736e1c612a114fecd
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-04-23 15:26:16 +0530

    Update SearchActionController.java

--- cures/src/main/java/controller/SearchActionController.java
+++ cures/src/main/java/controller/SearchActionController.java
@@ -1,7 +1,11 @@
 package controller;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.zip.GZIPOutputStream;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
