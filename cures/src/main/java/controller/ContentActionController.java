0e9b7644bc0cdee14ae9cc5605fecd8b9b637009
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-04-23 17:46:26 +0530

    Update ContentActionController.java

--- cures/src/main/java/controller/ContentActionController.java
+++ cures/src/main/java/controller/ContentActionController.java
@@ -387,7 +387,7 @@ public class ContentActionController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+	//	doGet(request, response);
 	}
 
 }
