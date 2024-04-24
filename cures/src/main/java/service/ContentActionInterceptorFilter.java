6512763c7166cab58b3ec1ce8ba6d6d518ef9219
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-04-24 16:47:55 +0530

    Update ContentActionInterceptorFilter.java

--- cures/src/main/java/service/ContentActionInterceptorFilter.java
+++ cures/src/main/java/service/ContentActionInterceptorFilter.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter("/c")
+@WebFilter("/IntegratedActionController")
 public class ContentActionInterceptorFilter implements Filter {
 
     @Override
@@ -19,41 +19,32 @@ public class ContentActionInterceptorFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         String userAgent = httpRequest.getHeader("User-Agent");
-   	 System.out.println("User-Agent"+ userAgent);
-   	if (userAgent != null && (userAgent.toLowerCase().contains("mobile") || userAgent.contains("AllCures"))) {
-	    // If it's a mobile device or MyApp, skip token validation
-	    return ;
-	}
+        System.out.println("User-Agent"+ userAgent);
+        if (userAgent != null && (userAgent.toLowerCase().contains("mobile") || userAgent.contains("AllCures"))) {
+            // If it's a mobile device or MyApp, skip token validation
+            chain.doFilter(request, response); // Continue with the filter chain
+            return;
+        }
         String uri = httpRequest.getRequestURI();
         String queryString = httpRequest.getQueryString();
-
         System.out.println("Intercepted URL: " + uri);
         System.out.println("queryString in  URL: " + queryString);
-        // Check if the request URL matches /SearchActionController and cmd parameter is set to getResults
- //       if ("/cures/content".equals(uri) && queryString != null && queryString.contains("cmd=getResults")) {
-        if ("/cures/content".equals(uri) && queryString != null ) {
+         // Check if the request URL matches /cure/content
+        if ("/cures/IntegratedActionController".equals(uri) && queryString != null) {
             // Your validation logic here
-            // Example: Check if a specific token exists in the request header
-        	System.out.println("hello");
+		
             String token = httpRequest.getHeader("Authorization");
- //           System.out.println(TokenValidator.isValidToken(token, uri));
+		 System.out.println("token: " + token);
+	    System.out.println(TokenValidator.isValidToken(token, uri));
+            
             try {
-                if (!(token == null )) {
-                    int result=TokenValidator.isValidToken(token,uri);
-                    if(result==0)
-                    {
-                    	httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or missing token.");
-                        return;
-                    }
-                    else if(result==2)
-                    {
-                    	httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "You have crossed the Maximum Attempts.");
-                    }
-                }
-                else
-                {
-                	httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or missing token.");
-                    return ;
+                if (token != null && TokenValidator.isValidToken(token, uri) == 1) {
+                    // Token is valid, continue with the filter chain
+                    chain.doFilter(request, response);
+                    return;
+                } else {
+                    httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or missing token.");
+                    return;
                 }
             } catch (Exception e) {
                 // Handle the exception
@@ -62,13 +53,15 @@ public class ContentActionInterceptorFilter implements Filter {
                 return;
             }
         }
-        
+
         // If the request does not match the specified criteria, continue processing without validation
-  //      chain.doFilter(request, response);
+        chain.doFilter(request, response);
     }
 
-    @Override
-    public void destroy() {
-        // Cleanup code (if needed)
-    }
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
