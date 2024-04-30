fda7f273060915e9fc97e3d8239b2d5f15d7b016
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-04-30 10:38:23 +0530

    Update TipDaoImpl.java

--- cures/src/main/java/dao/TipDaoImpl.java
+++ cures/src/main/java/dao/TipDaoImpl.java
@@ -62,8 +62,8 @@ public class TipDaoImpl {
 		{
           Query query = session
 				.createNativeQuery("INSERT INTO `tip`" + " (`tip_title`,"
-						+ " `tip_date`,"+"`user_id`,"+"`tip_status`,"+"`tip_updatedtime` , "+"`article_id`)"
-						+ " VALUES" + " ('" + tip_title + "',    " + "     '" + tip_date + "', " + " '"+ user_id +"', " + " '"+tip_status+"', " + " '"+tip_updatedtime+"' , " + " '"+article_id+"' );" + "");
+						+ " `tip_date`,"+"`user_id`,"+"`tip_status`, "+"`article_id`)"
+						+ " VALUES" + " ('" + tip_title + "',    " + "     '" + tip_date + "', " + " '"+ user_id +"', " + " '"+tip_status+"',  " + " '"+article_id+"' );" + "");
 		
 		try {
 			ret = query.executeUpdate();
