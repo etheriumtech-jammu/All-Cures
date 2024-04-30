20c5b38d005c2ba93326824c8d0e9fceb6fed1a6
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-04-30 12:58:50 +0530

    Update SolrIndexer.java

--- cures/src/main/java/util/SolrIndexer.java
+++ cures/src/main/java/util/SolrIndexer.java
@@ -1,8 +1,12 @@
 package util;
+
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrInputDocument;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,116 +18,105 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
 public class SolrIndexer {
-    public void indexFiles(String directoryPath, String solrUrl) throws IOException {
-        SolrClient solr = new HttpSolrClient.Builder(solrUrl).build();
 
-        traverseDirectory(new File(directoryPath), solr);
+    public void indexFiles(String directoryPath, String solrUrl) throws IOException, SolrServerException {
+        SolrClient solr = new HttpSolrClient.Builder(solrUrl).build();
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().toLowerCase().endsWith(".json")) {
+                    // Read the file
+                    StringBuilder content = new StringBuilder();
+                    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            content.append(line).append("\n");
+                        }
+                    }
+
+                    // Extract article ID from the filename
+                    int articleId = extractArticleId(file.getName());
+                    if (articleId == -1) {
+                        System.out.println("Invalid filename format: " + file.getName());
+                        continue; // Skip this file if filename format is invalid
+                    }
+
+                    // Build a Solr query to retrieve the document by its ID
+                    SolrQuery query = new SolrQuery();
+                    query.setQuery("id:" + articleId);
+
+                    // Execute the query
+                    QueryResponse response = solr.query(query);
+
+                    // Check if any documents are returned
+                    boolean documentExists = !response.getResults().isEmpty();
+                    JSONObject decryptedContent = decryptAndParse(content.toString());
+                    if (documentExists) {
+                        // Retrieve the existing document from the response
+                        SolrDocument existingDocument = response.getResults().get(0);
+
+                        if (decryptedContent != null) {
+                            // Add new content to the existing document
+                            existingDocument.addField("content", decryptedContent.toString());
+
+                            // Update the document in Solr
+                            solr.add(convertToSolrInputDocument(existingDocument));
+                        } else {
+                            System.out.println("Error decrypting and parsing content for file: " + file.getName());
+                            continue; // Skip this file if decryption or parsing fails
+                        }
+                    } else {
+                        System.out.println("Document with ID " + articleId + " does not exist in Solr.");
+                    }
+                }
+            }
+        }
 
         // Commit changes to the Solr index
-        try {
-            solr.commit();
-        } catch (org.apache.solr.client.solrj.SolrServerException e) {
-            e.printStackTrace();
-        }
+        solr.commit();
 
         // Close Solr client
         solr.close();
     }
 
-    private void traverseDirectory(File directory, SolrClient solr) throws IOException {
-        File[] files = directory.listFiles();
-
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    // Recursive call to traverse subdirectories
-                    traverseDirectory(file, solr);
-                } else if (file.isFile() && file.getName().toLowerCase().endsWith(".json")) {
-                    // Process JSON files
-                    processJsonFile(file, solr);
-                }
-            }
-        }
-    }
-
-    private void processJsonFile(File file, SolrClient solr) throws IOException {
-        // Read the file
-        StringBuilder content = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        // Create a Solr document for the file
-        SolrInputDocument document = new SolrInputDocument();
-        document.addField("id", file.getName()); // Unique identifier for the document
-        String decryptcontent;
-        try {
-            decryptcontent = decryptData(content.toString());
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            return;
-        }
-      
-        document.addField("content", decryptcontent); // File content
-        System.out.println(file.getName());
-
-        // Add the document to Solr
-        try {
-            solr.add(document);
-        } catch (org.apache.solr.client.solrj.SolrServerException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
-        String directoryPath = "/home/uat/Production/installers/tomcat/webapps/cures_articleimages"; // Directory containing files
-        String solrUrl = "http://localhost:8983/solr/articontent_core"; // Replace with your Solr collection URL
+        String directoryPath = "C:\\JAVA\\New"; // Directory containing files
+        String solrUrl = "http://localhost:8983/solr/test"; // Replace with your Solr collection URL
 
         SolrIndexer indexer = new SolrIndexer();
         try {
             indexer.indexFiles(directoryPath, solrUrl);
-        } catch (IOException e) {
+        } catch (IOException | SolrServerException e) {
             e.printStackTrace();
         }
     }
 
-   public String decryptData(String encryptedData) throws UnsupportedEncodingException {
+    public JSONObject decryptAndParse(String encodedContent) {
         try {
             // Decode URL-encoded data
-            String decodedData = URLDecoder.decode(encryptedData, "UTF-8");
+            String decodedData = URLDecoder.decode(encodedContent, "UTF-8");
 
-            // Convert the decoded data to JSON object
-            JSONObject json = new JSONObject(decodedData);
-
-            // Extract information from the JSON object
-            long time = json.getLong("time");
-            JSONArray blocks = json.getJSONArray("blocks");
-
-            // Iterate over the blocks and decrypt each one
-            StringBuilder decryptedText = new StringBuilder();
-            for (int i = 0; i < blocks.length(); i++) {
-                JSONObject block = blocks.getJSONObject(i);
-                if (block.has("data")) {
-                    JSONObject data = block.getJSONObject("data");
-                    if (data.has("text")) {
-                        String text = data.getString("text");
-                        // Perform decryption here (if needed)
-                        decryptedText.append(text).append("\n");
-                    }
-                }
-            }
-
-            // Return the decrypted text or process it further
-            return decryptedText.toString();
-        } catch (JSONException e) {
+            // Parse the decoded JSON string into a JSONObject
+            return new JSONObject(decodedData);
+        } catch (UnsupportedEncodingException | JSONException e) {
             e.printStackTrace();
-            return "Error occurred while decrypting the data.";
+            return null; // Handle decoding or parsing errors
         }
+    }
+
+    private int extractArticleId(String filename) {
+        try {
+            String idStr = filename.substring(filename.lastIndexOf('_') + 1, filename.lastIndexOf('.'));
+            return Integer.parseInt(idStr);
+        } catch (NumberFormatException | StringIndexOutOfBoundsException e) {
+            return -1; // Invalid filename format
+        }
+    }
+
+    private SolrInputDocument convertToSolrInputDocument(SolrDocument solrDocument) {
+        SolrInputDocument inputDocument = new SolrInputDocument();
+        solrDocument.forEach(inputDocument::addField);
+        return inputDocument;
     }
 }
