a870c0a0927bb7bd020b072936f10cb33d73fbd3
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-05-01 15:33:24 +0530

    Update SolrIndexer.java

--- cures/src/main/java/util/SolrIndexer.java
+++ cures/src/main/java/util/SolrIndexer.java
@@ -21,8 +21,8 @@ public class SolrIndexer {
 
     public void indexFiles(String directoryPath, String solrUrl) throws IOException, SolrServerException {
         SolrClient solr = new HttpSolrClient.Builder(solrUrl).build();
-        String directory="/home/uat/Production/installers/tomcat/webapps/cures_articleimages/3/2021/12/23";
-        File directory = new File(directory);
+        String directory1="/home/uat/Production/installers/tomcat/webapps/cures_articleimages/3/2021/12/23";
+        File directory = new File(directory1);
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
