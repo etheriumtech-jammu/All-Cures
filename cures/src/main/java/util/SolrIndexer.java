b4940d1d5566a25961c1aac786f24ceb0ad0daa1
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-05-01 16:17:01 +0530

    Update SolrIndexer.java

--- cures/src/main/java/util/SolrIndexer.java
+++ cures/src/main/java/util/SolrIndexer.java
@@ -20,67 +20,83 @@ import java.net.URLDecoder;
 public class SolrIndexer {
 
     public void indexFiles(String directoryPath, String solrUrl) throws IOException, SolrServerException {
-        SolrClient solr = new HttpSolrClient.Builder(solrUrl).build();
-        String directory1="/home/uat/Production/installers/tomcat/webapps/cures_articleimages/50/2023/03/24";
-        File directory = new File(directory1);
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isFile() && file.getName().toLowerCase().endsWith(".json")) {
-                    // Read the file
-                    StringBuilder content = new StringBuilder();
-                    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            content.append(line).append("\n");
-                        }
-                    }
+	    SolrClient solr = new HttpSolrClient.Builder(solrUrl).build();
+	    String directory1 = "/home/uat/Production/installers/tomcat/webapps/cures_articleimages/50/2023/03/24";
+	    File directory = new File(directory1);
+	    File[] files = directory.listFiles();
 
-                    // Extract article ID from the filename
-                    int articleId = extractArticleId(file.getName());
-                  
-                    if (articleId == -1) {
-                        System.out.println("Invalid filename format: " + file.getName());
-                        continue; // Skip this file if filename format is invalid
-                    }
+	    System.out.println(files.length);
+	    if (files != null) {
+	        for (File file : files) {
+	            if (file.isFile() && file.getName().toLowerCase().endsWith(".json")) {
+	                // Read the file
+	                StringBuilder content = new StringBuilder();
+	                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+	                    String line;
+	                    while ((line = reader.readLine()) != null) {
+	                        content.append(line).append("\n");
+	                    }
+	                }
 
-                    // Build a Solr query to retrieve the document by its ID
-                    SolrQuery query = new SolrQuery();
-                    query.setQuery("article_id:" + articleId);
+	                // Extract article ID from the filename
+	                int articleId = extractArticleId(file.getName());
+	                System.out.println(" filename format: " + file.getName());
+	                if (articleId == -1) {
+	                    System.out.println("Invalid filename format: " + file.getName());
+	                    continue; // Skip this file if filename format is invalid
+	                }
 
-                    // Execute the query
-                    QueryResponse response = solr.query(query);
+	                // Build a Solr query to retrieve the document by its ID
+	                SolrQuery query = new SolrQuery();
+	                query.setQuery("id:" + articleId);
 
-                    // Check if any documents are returned
-                    boolean documentExists = !response.getResults().isEmpty();
-                    JSONObject decryptedContent = decryptAndParse(content.toString());
-                    if (documentExists) {
-                        // Retrieve the existing document from the response
-                        SolrDocument existingDocument = response.getResults().get(0);
+	                // Execute the query
+	                QueryResponse response = solr.query(query);
 
-                        if (decryptedContent != null) {
-                            // Add new content to the existing document
-                            existingDocument.setField("content_new", decryptedContent.toString());
+	                // Check if any documents are returned
+	                boolean documentExists = !response.getResults().isEmpty();
+	                JSONObject decryptedContent = decryptAndParse(content.toString());
+	                if (documentExists) {
+	                    // Retrieve the existing document from the response
+	                    SolrDocument existingDocument = response.getResults().get(0);
 
-                            // Update the document in Solr
-                            solr.add(convertToSolrInputDocument(existingDocument));
-                        } else {
-                            System.out.println("Error decrypting and parsing content for file: " + file.getName());
-                            continue; // Skip this file if decryption or parsing fails
-                        }
-                    } else {
-                        System.out.println("Document with ID " + articleId + " does not exist in Solr.");
-                    }
-                }
-            }
-        }
+	                    if (decryptedContent != null) {
+	                        // Add new content to the existing document
+	                        existingDocument.setField("content", decryptedContent.toString());
 
-        // Commit changes to the Solr index
-        solr.commit();
+	                        // Check if the content_new field exists and add it to the document
+	                        if (existingDocument.getFieldValue("content_new") == null) {
+	                            existingDocument.setField("content_new", decryptedContent.toString());
+	                        }
 
-        // Close Solr client
-        solr.close();
-    }
+	                        // Retrieve and print the content_new field from the document
+	                        Object contentNewFieldValue = existingDocument.getFieldValue("content_new");
+	                        if (contentNewFieldValue != null) {
+	                            System.out.println("content_new: " + contentNewFieldValue.toString());
+	                        } else {
+	                            System.out.println("content_new not found in the document.");
+	                        }
+
+	                        // Update the document in Solr
+	                        solr.add(convertToSolrInputDocument(existingDocument));
+	                    } else {
+	                        System.out.println("Error decrypting and parsing content for file: " + file.getName());
+	                        continue; // Skip this file if decryption or parsing fails
+	                    }
+	                } else {
+	                    System.out.println("Document with ID " + articleId + " does not exist in Solr.");
+	                }
+	            }
+	        }
+	    }
+
+	    // Commit changes to the Solr index
+	    solr.commit();
+
+	    // Close Solr client
+	    solr.close();
+	}
+
 
     public static void main(String[] args) {
         String directoryPath = "C:\\JAVA\\New"; // Directory containing files
