77fb9186564e5d2c558ed993188f8c4f97aff9c7
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-05-03 12:28:17 +0530

    Update SplSearchDaoImpl.java
    Add closing of SolrClient

--- cures/src/main/java/dao/SplSearchDaoImpl.java
+++ cures/src/main/java/dao/SplSearchDaoImpl.java
@@ -46,7 +46,12 @@ public class SplSearchDaoImpl {
 		} catch (SolrServerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
+		}finally {
+	        try {
+	            if (client != null) {
+	                client.close(); // Close the SolrClient instance
+	            }
+	        } catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
