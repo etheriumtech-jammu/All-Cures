d6ca6fa7dcb4118fccf0e39ef1d4cf3194233d1c
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-05-17 14:06:37 +0530

    Update SearchController.java

--- cures/src/main/java/controller/SearchController.java
+++ cures/src/main/java/controller/SearchController.java
@@ -1,7 +1,17 @@
 package controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
