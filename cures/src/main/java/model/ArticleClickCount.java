085e3deafee8dbbf3b5feb07328c4929f33b44ce
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-06-04 14:25:35 +0530

    Create ArticleClickCount.java

---
+++ cures/src/main/java/model/ArticleClickCount.java
@@ -0,0 +1,55 @@
+package model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class ArticleClickCount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "article_id", nullable = false)
+    private Long articleID;
+
+    @Column(name = "click_date", nullable = false)
+    private LocalDate clickDate;
+
+    @Column(name = "click_count", nullable = false)
+    private Long clickCount;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getArticleId() {
+        return articleID;
+    }
+
+    public void setArticleId(Long articleId) {
+        this.articleID = articleId;
+    }
+
+    public LocalDate getClickDate() {
+        return clickDate;
+    }
+
+    public void setClickDate(LocalDate clickDate) {
+        this.clickDate = clickDate;
+    }
+
+    public Long getClickCount() {
+        return clickCount;
+    }
+
+    public void setClickCount(Long clickCount) {
+        this.clickCount = clickCount;
+    }
+}
