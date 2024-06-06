4c06a57cb7f1ac49ca23532716ff2d361eab5267
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-06-06 10:41:51 +0530

    Update AppController.java

--- cures/src/main/java/controller/AppController.java
+++ cures/src/main/java/controller/AppController.java
@@ -9,7 +9,10 @@ public class AppController {
     public String handleRequest1() {
         return "success"; // The view name (index.jsp)
     }
-    
+     @GetMapping("/*")
+    public String handleRequest3() {
+        return "success"; // The view name (index.jsp)
+    }
     @GetMapping("/webstories")
     public String handleRequest2() {
         return "success"; // The view name (index.jsp)
