e404d3e42974b3bdbe5e2c2357abc5f6c3415b9e
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-06-18 12:36:59 +0530

    Update AppController.java

--- cures/src/main/java/controller/AppController.java
+++ cures/src/main/java/controller/AppController.java
@@ -37,7 +37,7 @@ public class AppController {
 	    }
     
    
-	 @GetMapping({"/searchmedicine/medicinetype/*", "searchcategory/disease/*","/webstories","/doctor/*"})
+	 @GetMapping({"/searchmedicine/medicinetype/*", "searchcategory/disease/*","/webstories","/doctor/*","/user/profile"})
     public String handleRequest1(Model model) {
 		 System.out.println("webstories");
 	    String title = "All-Cures - Around health and about it";
