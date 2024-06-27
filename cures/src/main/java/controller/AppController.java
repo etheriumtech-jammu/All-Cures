2e77e493e1025b2aa4a1728515cd22ed99754eeb
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-06-27 13:37:21 +0530

    Update AppController.java

--- cures/src/main/java/controller/AppController.java
+++ cures/src/main/java/controller/AppController.java
@@ -52,6 +52,8 @@ public class AppController {
             Query query = session.createNativeQuery("SELECT title, description FROM article WHERE article_id = :articleId");
             query.setParameter("articleId", article_id);
             List<Object[]> articleList = query.getResultList();
+	    System.out.println(articleList.size());
+            
             // If article is found, update title and description
             if (!articleList.isEmpty()) {
                 Object[] obj = articleList.get(0);
