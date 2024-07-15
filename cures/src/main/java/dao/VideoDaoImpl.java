a4ff6abc4e974ce8a49714c5163d599deb319d1d
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-07-15 12:52:58 +0530

    Update VideoDaoImpl.java

--- cures/src/main/java/dao/VideoDaoImpl.java
+++ cures/src/main/java/dao/VideoDaoImpl.java
@@ -642,7 +642,7 @@ public class VideoDaoImpl {
 	            EmailDTO docEmailDTO = new EmailDTO();
 	            docEmailDTO.setTo(docEmail);
 	            docEmailDTO.setSubject("Video Consultation Appointment Confirmation");
-
+		    docEmailDTO.setFrom("All-Cures INFO <info@all-cures.com>");
 	            Map<String, Object> docTemplateData = new HashMap<>();
 	            docTemplateData.put("templatefile", "email/video.ftlh");
 	            docTemplateData.put("videoChatLink", docLink);
@@ -665,7 +665,7 @@ public class VideoDaoImpl {
 	            EmailDTO userEmailDTO = new EmailDTO();
 	            userEmailDTO.setTo(userEmail);
 	            userEmailDTO.setSubject("Video Consultation Appointment Confirmation");
-
+		    userEmailDTO.setFrom("All-Cures INFO <info@all-cures.com>");
 	            Map<String, Object> userTemplateData = new HashMap<>();
 	            userTemplateData.put("templatefile", "email/video_user.ftlh");
 	            userTemplateData.put("videoChatLink", userLink);
