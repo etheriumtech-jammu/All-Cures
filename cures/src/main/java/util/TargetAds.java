5682aaacdb03e4250a792553318a160a911c042f
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-07-17 16:06:01 +0530

    Update TargetAds.java

--- cures/src/main/java/util/TargetAds.java
+++ cures/src/main/java/util/TargetAds.java
@@ -279,6 +279,128 @@ public class TargetAds {
 			      DailyTaskScheduler.displayRotatedAds(ads, rotationCount,AdCount,AdURL,Integer.parseInt(parts[1]));
 		        }
 	}
+
+	public static void update_med_type() throws JsonProcessingException
+	{
+		HashMap<String, ArrayList<Integer>> adsByCondition = new HashMap<>();
+		   
+		   Integer medType=0;
+		   LinkedHashMap<Integer, Integer> D_AdCount = new LinkedHashMap<>();
+		   LinkedHashMap<String, String> AdURL = new LinkedHashMap<>();
+		   Set<String> medTypeSet = null;
+		   int rotationCount=0;
+			Session session = HibernateUtil.buildSessionFactory();
+	  	  int adCountPerDay=0;
+			Query query = session.createNativeQuery(
+					"SELECT AdID,ImageLocation, StartDate, EndDate,AdCount,(AdCount-AdDelivered), AdTypeName,MedTypeID\r\n"
+					+ "				FROM campaignads c join adstypes t\r\n"
+					+ "				WHERE CURDATE() BETWEEN StartDate AND EndDate and ReviewStatus=1 and c.AdTypeID=t.AdTypeID and MedTypeID!=0");
+			List<Object[]> results = (List<Object[]>) query.getResultList();
+			System.out.println(results.size());
+			List<Object[]> arrayDataList = new ArrayList<>();
+			for (Object[] objects : results) {
+			    Integer AdID = (Integer) objects[0];
+			    
+			    String ImageLocation = (String) objects[1];
+			    Date StartDate=(Date)objects[2];
+			    Date EndDate=(Date)objects[3];
+			   Integer Count = (Integer) objects[4];
+			   BigInteger AdCount=(BigInteger) objects[5] != null ? (BigInteger) objects[5] : new BigInteger(Count.toString());
+			   String AdType=(String)objects[6];
+			   medType=(Integer) objects[7];
+			    Object[] dataArray = new Object[]{AdID, ImageLocation,StartDate,EndDate,AdCount};
+			    arrayDataList.add(dataArray);
+			    adCountPerDay = calculateAdCountPerDay(arrayDataList);
+			    
+			   		    D_AdCount.put(AdID,adCountPerDay);
+			   		    AdURL.put("Banner_" + medType + "_"+ AdID,ImageLocation);
+//			    HashMap<Integer, ArrayList<Integer>> adsByCondition = new HashMap<>();
+			    
+			        if (!adsByCondition.containsKey("MedType_" +medType)) {
+			            adsByCondition.put("MedType_" + medType, new ArrayList<>());
+			        }
+			        adsByCondition.get("MedType_" + medType).add(AdID);
+			       
+
+			        // Iterate through the keys and add them to the List
+			        medTypeSet = new HashSet<>(adsByCondition.keySet());
+			     // Print D_AdCount, adsByCondition, and dcCondList
+			        System.out.println("D_AdCount: " + D_AdCount);
+			        System.out.println("adsByCondition: " + adsByCondition);
+			        System.out.println("dcCondSet: " + medTypeSet);
+			        
+			    }
+					 HashMap<String, HashMap<Integer, Integer>> resultMap = new HashMap<>();
+		
+					 medTypeSet = new HashSet<>(adsByCondition.keySet());
+			 Iterator<String> iterator = medTypeSet.iterator();
+		        while (iterator.hasNext()) {
+		            String condition = iterator.next();
+
+		            // Create a new valuesAndCounts map for each condition
+		            HashMap<Integer, Integer> valuesAndCounts = new HashMap<>();
+		            
+		            ArrayList<Integer> adIDs = adsByCondition.get(condition);
+
+		            for (Integer adID : adIDs) {
+		                valuesAndCounts.put(adID, D_AdCount.get(adID));
+		            }
+
+		            // Store the valuesAndCounts map in resultMap
+		            resultMap.put(condition, new HashMap<>(valuesAndCounts));
+		            
+		        }
+		        // Print resultMap
+		       
+		        
+		        for (Map.Entry<String, HashMap<Integer, Integer>> entry : resultMap.entrySet()) {
+		            String condition = entry.getKey();
+		            Set<String> keyList = new HashSet<>();
+		            HashMap<Integer, Integer> valuesCounts = entry.getValue();
+		            LinkedHashMap<String, Integer> AdCount = new LinkedHashMap<>();
+		            System.out.println("Condition: " + condition);
+		            String[] parts = condition.split("_");
+		            System.out.println("hhh" + parts[1]);
+		            for (HashMap.Entry<Integer, Integer> subEntry : valuesCounts.entrySet()) {
+		                Integer adID = subEntry.getKey();
+		                Integer count = subEntry.getValue();
+		               rotationCount=rotationCount +count;
+		               System.out.println("  AdID: " + adID + ", Count: " + count);
+		                AdCount.put("Banner_" + parts[1] + "_"+ adID, count);
+		                keyList.add("Banner_" + parts[1] + "_"+ adID);
+		                System.out.println(AdCount);    
+		            }
+		            System.out.println("KEYLIST"+keyList);
+		            List<String> ads = new ArrayList<>();
+		            if(AdCount.size() ==1)
+		      	       {
+		      	    	   System.out.println("Single AD");
+		      	    	 for ( String key1 : keyList) {	 
+		      	    		System.out.println(key1);
+		      	    		 Integer count = AdCount.get(key1);
+		      	    		 rotationCount=count;
+		      	    		 for (int i = 0; i < count; i++) {
+		      	    			ads.add(key1);
+		      	    		 }}
+		      	       }
+		      	       
+		            else
+		            {
+		            LinkedHashMap<String, Double> Result= SimplifiedRatioCalculator.CalculateRatio(AdCount);
+			           System.out.println("Size:"+AdCount.size());  
+		            
+			            ads =DynamicAdPattern.generateAds(Result);
+			            System.out.println(ads);
+		            }
+		          
+		            System.out.println("AdURL"+AdURL);
+		            DC_AdCount.put(parts[1],rotationCount+ ":0");
+		            System.out.println("DC_AdCount"+DC_AdCount);
+		            System.out.println("rotationCount"+rotationCount);
+		            
+			      DailyTaskScheduler.displayRotatedAds(ads, rotationCount,AdCount,AdURL,Integer.parseInt(parts[1]));
+		        }
+	}
 	
 	
 public static int calculateAdCountPerDay(List<Object[]> ad) {
