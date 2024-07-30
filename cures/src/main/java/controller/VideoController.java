6f606d5dca8d96ae06e444c8aaba2c3b452e8cd3
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-07-30 13:52:57 +0530

    Update VideoController.java

--- cures/src/main/java/controller/VideoController.java
+++ cures/src/main/java/controller/VideoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import dao.SponsoredAdsDaoImpl;
 import dao.VideoDaoImpl;
 import model.AvailabilitySchedule;
