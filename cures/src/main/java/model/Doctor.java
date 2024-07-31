0884274baffb3d70c19e4940c82742bb309ff01c
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-07-31 12:40:38 +0530

    Update Doctor.java

--- cures/src/main/java/model/Doctor.java
+++ cures/src/main/java/model/Doctor.java
@@ -39,7 +39,7 @@ public class Doctor {
 			String other_spls, String sub_spls, String address1, String address2, String city, String state,
 			String country_code, String over_allrating, String create_date, Integer docactive, String prefix,
 			String docname_first, String docname_middle, String docname_last, String name, String email, String waiting_time,
-		 String about,String img_Loc,String MedicineType,Integer videoService) {
+		 String about,String img_Loc,String MedicineType) {
 		super();
 		this.docID = DocID;
 		this.gender = gender;
@@ -68,7 +68,7 @@ public class Doctor {
 		this.waitingTime = waiting_time;
 		this.medicineType=MedicineType;
 		this.about = about;
-		this.videoService=videoService;
+		
 	}
 	
 	//new sor document with lesser fields
