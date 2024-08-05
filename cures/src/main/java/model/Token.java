36f00816c97ecebd01d9aae84a93217370136d2f
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-08-05 14:02:10 +0530

    Update Token.java

--- cures/src/main/java/model/Token.java
+++ cures/src/main/java/model/Token.java
@@ -63,11 +63,11 @@ public class Token {
 		this.lastUpdatedDate = lastUpdatedDate;
 	}
 
-	public int getStatus() {
+	public Integer getStatus() {
 		return status;
 	}
 
-	public void setStatus(int status) {
+	public void setStatus(Integer status) {
 		this.status = status;
 	}
 
