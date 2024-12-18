0e3bcc7b0357ef60c7ba18161c4be8aec00a699f
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-12-18 11:52:11 +0530

    Update DataController.java

--- cures/src/main/java/controller/DataController.java
+++ cures/src/main/java/controller/DataController.java
@@ -63,21 +63,29 @@ public class DataController {
 		
 	}
 
-	  @RequestMapping(value = "/WebSocketStart", method = RequestMethod.GET)
+	@RequestMapping(value = "/startWebSocketServer", method = RequestMethod.GET)
 	    @ResponseBody
 	public String startWebSocketServer() {
-  try {
-//    WebSocketImpl.DEBUG = true;
-    int port = 8000; // port number
-    SocketIOServer1 s = new SocketIOServer1(port);
-      s.start();
-	System.out.println("ChatServer started on port: " + s.getPort());
-	return "WebSocket server started!";
-  }catch (Exception e) {
-      e.printStackTrace();
-      return "Failed to start WebSocket server: " + e.getMessage();
-  }
- }
+//	  try {
+//	    WebSocketImpl.DEBUG = true;
+	    int port = 8000; // port number
+	    SocketIOServer1 s = new SocketIOServer1(port);
+	      s.start();
+		System.out.println("ChatServer started on port: " + s.getPort());
+/*	    if (!isRunning == true) {
+	    	SocketIOServer1 s = new SocketIOServer1(port);
+	      s.start();
+	      isRunning = true;
+	   
+	      System.out.println("ChatServer started on port: " + s.getPort());
+	    } else {
+	      System.out.println("ChatServer continues on port: " );
+		  }
+*/
+	    new ClientExample();
+
+	  return "WebSocket server started!";
+	  }
 
 	@RequestMapping(value = "/newsletter/upload", produces = "application/json", method = RequestMethod.POST)
 	public int NewsLetter_fileupload(@RequestParam("image") CommonsMultipartFile image) throws IOException {
