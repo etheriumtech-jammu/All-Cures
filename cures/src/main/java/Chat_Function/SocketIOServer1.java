2a637743277fdd5a7adca02398933de1f3eff732
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2024-12-19 16:13:42 +0530

    Update SocketIOServer1.java

--- cures/src/main/java/Chat_Function/SocketIOServer1.java
+++ cures/src/main/java/Chat_Function/SocketIOServer1.java
@@ -72,7 +72,7 @@ public class SocketIOServer1 extends WebSocketServer {
     }
 
     @Override
-    public void stop() throws IOException, InterruptedException {
+    public void stop() throws InterruptedException {
         super.stop();
         running = false;
         SocketIOServer1.serverInstance = null; // Reset singleton instance
