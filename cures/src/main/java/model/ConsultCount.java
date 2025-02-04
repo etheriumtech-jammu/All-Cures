a05daeec8a9979aa28c39e5e1dea2ae25c8a63bf
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2025-02-04 11:54:37 +0530

    Create ConsultCount.java

---
+++ cures/src/main/java/model/ConsultCount.java
@@ -0,0 +1,41 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ConsultCount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer count;
+  
+    // Constructors
+    public ConsultCount(Integer count) {
+    	this.count=count;
+    }
+
+    public ConsultCount(Long id, Integer count) {
+        this.id = id;
+        this.count = count;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
