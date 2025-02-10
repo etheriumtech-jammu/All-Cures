052eb0d02157b4ca68c5f429145fa5f07868ac84
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2025-02-10 16:49:08 +0530

    Update VideoDaoImpl.java

--- cures/src/main/java/dao/VideoDaoImpl.java
+++ cures/src/main/java/dao/VideoDaoImpl.java
@@ -690,7 +690,7 @@ public class VideoDaoImpl {
 	        }
 	    }
 
-	public static  List<HashMap<String, Object>> getDoctors(Integer offset, Integer medTypeID) {
+	 public static HashMap<String, Object> getDoctors(Integer offset, Integer medTypeID) {
 	        Session session = HibernateUtil.buildSessionFactory();
 	        	
 			/*	        Query query1 = session.createNativeQuery("SELECT d.docid, d.prefix, d.docname_first, d.docname_middle, d.docname_last,d.img_Loc " +
@@ -861,8 +861,11 @@ public class VideoDaoImpl {
                 doctorList.add(doctor);	
                 
                 }
-            doctorList.add(totalPagesCount);
-	        return doctorList;
+           // **Wrap the data in a single response object**
+            HashMap<String, Object> response = new HashMap<>();
+            response.put("data", doctorList);
+            response.put("totalPagesCount", totalPagesCount);
+	        return response;
 	    }
 	public static  List<HashMap<String, Object>> getDoctorsList(Integer offset) {
 	        Session session = HibernateUtil.buildSessionFactory();
