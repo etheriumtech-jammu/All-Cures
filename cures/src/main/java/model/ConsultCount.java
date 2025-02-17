4e6d6fd96f8ef573acf9e07e3abb981620c5e312
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2025-02-17 10:42:27 +0530

    Update ConsultCount.java

--- cures/src/main/java/model/ConsultCount.java
+++ cures/src/main/java/model/ConsultCount.java
@@ -5,21 +5,37 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+
 @Entity
 public class ConsultCount {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(nullable = false)
+    private Integer userID;
+    
+    @Column(nullable = false)
     private Integer count;
   
+    @Column(name = "updated_at")  
+    private LocalDateTime updatedAt;
+
     // Constructors
-    public ConsultCount(Integer count) {
-    	this.count=count;
+    public ConsultCount(Integer userID, Integer count,LocalDateTime updatedAt) {
+        this.userID = userID;
+        this.count = count;
+        this.updatedAt=updatedAt;
     }
 
-    public ConsultCount(Long id, Integer count) {
+    public ConsultCount(Long id, Integer userID, Integer count, LocalDateTime updatedAt) {
         this.id = id;
+        this.userID = userID;
         this.count = count;
+        this.updatedAt=updatedAt;
     }
 
     // Getters and Setters
@@ -31,6 +47,14 @@ public class ConsultCount {
         this.id = id;
     }
 
+    	public Integer getUserId() {
+        return userID;
+    }
+
+    public void setUserId(Integer userID) {
+        this.userID = userID;
+    }
+
     public Integer getCount() {
         return count;
     }
@@ -38,4 +62,14 @@ public class ConsultCount {
     public void setCount(Integer count) {
         this.count = count;
     }
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+    
+    
 }
