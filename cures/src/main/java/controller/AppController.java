64bd8e716d684ada43b3fc3c9ab87ccb26585920
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2025-06-04 11:36:34 +0530

    Update AppController.java

--- cures/src/main/java/controller/AppController.java
+++ cures/src/main/java/controller/AppController.java
@@ -26,7 +26,7 @@ public class AppController {
 	    }
     
    
-	 @GetMapping({"/searchmedicine/medicinetype/*", "searchcategory/disease/*","/webstories","/doctor/*","/user/profile","/searchcures/*","/searchName/*","/search/*","/loginForm/verify","/loginForm/ResetPass/*","/paymentStatus","/statusPayment","/notification/mensi.daily.co/*"})
+	 @GetMapping({"/searchmedicine/medicinetype/*", "searchcategory/disease/*","/webstories","/doctor/*","/user/profile","/searchcures/*","/searchName/*","/search/*","/loginForm/verify","/doctor-connect/*","/loginForm/ResetPass/*","/paymentStatus","/statusPayment","/notification/mensi.daily.co/*"})
     public String handleRequest1(Model model) {
 	    String title = "All-Cures: Natural Remedies & Holistic Health Solutions";
         String desc = "Discover expert-backed natural remedies, personalized wellness plans, and traditional medical insights from Ayurveda, Unani, Chinese, Persian, and more. Heal the natural way with All-Cures.";
