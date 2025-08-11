af946d4ca060bc8de7c959d65e5a858d68a78ca3
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2025-08-11 11:21:11 +0530

    Update AppointmentController.java

--- cures/src/main/java/controller/AppointmentController.java
+++ cures/src/main/java/controller/AppointmentController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
