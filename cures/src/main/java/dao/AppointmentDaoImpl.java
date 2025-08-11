0e822c521076999b4f33e41128dca277a3fc919d
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2025-08-11 13:25:54 +0530

    Update AppointmentDaoImpl.java

--- cures/src/main/java/dao/AppointmentDaoImpl.java
+++ cures/src/main/java/dao/AppointmentDaoImpl.java
@@ -354,7 +354,9 @@ public class AppointmentDaoImpl {
 
 	            for (LocalDate date = today; !date.isAfter(end); date = date.plusDays(1)) {
 	                DayOfWeek dayOfWeek = date.getDayOfWeek();
-					
+					  System.out.println((isDoctorAvailableOnDay(session, doctorId, dayOfWeek)));  
+					  System.out.println(("is Available");               
+	   	         
 	                if (isDoctorAvailableOnDay(session, doctorId, dayOfWeek)) {
 	                    TreeSet<LocalTime> bookedSlotsTime = getAppointmentsStartTimesForDate(doctorId, date);
 						
