7bbb16931ca339688870992938833870fc6075a0
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2025-08-13 16:35:33 +0530

    Update DailyCoService.java

--- cures/src/main/java/service/DailyCoService.java
+++ cures/src/main/java/service/DailyCoService.java
@@ -26,8 +26,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class DailyCoService {
 
-   @Value("${dailyco.api.key}")
-	private String dailyCoApiKey;
+    private String dailyCoApiKey = "cd370e4acc4f13598df4c87fcab72a2b4257c81a9e8bec9926481129d6d6f99d";
     private final String DAILY_CO_API_BASE_URL = "https://api.daily.co/v1";
     private final RestTemplate restTemplate;
 
