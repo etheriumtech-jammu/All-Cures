f763bcc1188c6d4f3bedda4b2401ddfb4244d99f
etheriumtech-jammu <80304078+etheriumtech-jammu@users.noreply.github.com>
2026-04-29 12:18:23 +0530

    Update GlobalExceptionHandler.java

--- cures/src/main/java/exception/GlobalExceptionHandler.java
+++ cures/src/main/java/exception/GlobalExceptionHandler.java
@@ -1,19 +1,39 @@
 package exception;
+import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import dto.ErrorResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(OtpException.class)
-    public ResponseEntity<?> handleOtpException(OtpException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
-    }
+	  @ExceptionHandler(OtpException.class)
+	    public ResponseEntity<?> handleOtpException(OtpException ex) {
+		  System.out.println("Handling OtpException: " + ex.getMessage());
+	        ErrorResponse error = new ErrorResponse(
+	                "OTP_ERROR",
+	                ex.getMessage()
+	        );
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleGeneral(Exception ex) {
-        return ResponseEntity.internalServerError()
-                .body("Something went wrong");
-    }
+	        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	    }
+
+	    @ExceptionHandler(Exception.class)
+	    public ResponseEntity<?> handleGenericException(Exception ex) {
+
+	        ErrorResponse error = new ErrorResponse(
+	                "INTERNAL_ERROR",
+	                "Something went wrong"
+	        );
+
+	        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	    
+//	    @PostConstruct
+//	    public void init() {
+//	        System.out.println("GlobalExceptionHandler Loaded");
+//	    }
 }
