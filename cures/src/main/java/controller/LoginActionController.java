8fda62ab09d82272de48eb2809f87f9f11cf9579
Divya Sharma <115102098+DivuShubhu@users.noreply.github.com>
2026-05-27 11:06:01 +0530

    Adding cookies

--- cures/src/main/java/controller/LoginActionController.java
+++ cures/src/main/java/controller/LoginActionController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;
@@ -14,18 +13,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import service.JWTTokenValidationInterceptor;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.json.JSONObject;
+
 import dao.RegistrationDaoImpl_New;
 import model.Registration;
-import dao.FCMDao;
 import util.Constant;
 import util.CookieManager;
 import util.Encryption;
 import util.EnDeCryptor;
-
+import service.JWTTokenValidationInterceptor;
 /**
  * Servlet implementation class LoginActionController
  */
@@ -33,7 +31,7 @@ import util.EnDeCryptor;
 public class LoginActionController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	public static final String SALT = "my-salt-text";
-	
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -50,14 +48,6 @@ public class LoginActionController extends HttpServlet {
 		Encryption encrypt = new Encryption();
 		CookieManager cook = new CookieManager();
 		String idcook =null;
-		// Reading data from the body of a POST request
-	    StringBuilder stringBuilder = new StringBuilder();
-	    BufferedReader reader = request.getReader();
-	    String line;
-	    while ((line = reader.readLine()) != null) {
-	        stringBuilder.append(line);
-	    }
-	    String body = stringBuilder.toString();
 
 		response.setContentType("text/html");
 		String email = request.getParameter(Constant.EMAIL);
@@ -66,16 +56,20 @@ public class LoginActionController extends HttpServlet {
 		Constant.log("????????????????????????::::::::::::::"+destinationUrl, 0);
 		//HttpSession session = request.getSession();
 		String value= JWTTokenValidationInterceptor.generateJWTToken(email);
+
 		String hashedPassword = null;
-		final String secretKey = Constant.SECRETE;		
-		hashedPassword = encrypt.encrypt(saltedPassword, secretKey);		
+		final String secretKey = Constant.SECRETE;
+		hashedPassword = encrypt.encrypt(saltedPassword, secretKey);
 		PrintWriter out = response.getWriter();
 
 		String remme= (request.getParameter(Constant.REMPWD) == null || "".equals(request.getParameter(Constant.REMPWD))) ? Constant.OFF : (String) request.getParameter(Constant.REMPWD);
 		//ToDo: This implementation should not be static as this will cause overwrite issues in a multi user environment
 		Registration user = RegistrationDaoImpl_New.findAllUsers(email, hashedPassword);
+
+
 		if(user != null){
 
+			user.setValue(value);
 			//Logging Password in Logs only in DEBUG Mode
 			Constant.log("Found at least one user with:"+email+" and pass combination"+hashedPassword, 0);
 			if (null != user.getLogin_attempt() && user.getLogin_attempt() > Constant.login_attempts_max) {
@@ -85,36 +79,23 @@ public class LoginActionController extends HttpServlet {
 				out.write("Maximum login attemps limit crossed, Please contact ADMIN");
 				out.flush();
 			}
-			
+
 			RegistrationDaoImpl_New.resetLoginDetails(user.getRegistration_id());
-			//Calling method to send the token to the user
-			 // Parse the JSON body and extract the value of the key "FCM"
-			 System.out.println("body"+body);
-		    if(body != null && !body.isEmpty()) {
-		    	 JSONObject jsonObject = new JSONObject(body);
-			    System.out.println(jsonObject);
-		 	    String fcmValue = jsonObject.getString("FCM");
-				   String deviceType=jsonObject.getString("deviceType");
-		 	    // Print the value of "FCM"
-		 	    System.out.println("FCM value: " + fcmValue);
-		 	   FCMDao.Token_Add(fcmValue, user.getRegistration_id(),deviceType);
-		    }
-		   
-			user.setValue(value);
-			
+
+
 			//User should be logged in now
 			session.setAttribute(Constant.USER, user);
 
 			if(remme.equalsIgnoreCase(Constant.ON)){
 				//Cookie[] cookies = nul
 				Constant.log("Remember Me On So Storing Cookies", 0);
-				//Poor Coding here storing the email and password in the Cookie itself; The presence of the cookie itself is an indicator 
+				//Poor Coding here storing the email and password in the Cookie itself; The presence of the cookie itself is an indicator
 				//that you should be seamlessly signed in and maybe what we put into the cookie is your id for lookup purposes, that too encrypted
 				//TODO: Fix based on above
 				//idcook=cook.storeCookiee(email, hashedPassword);
-				//FUCP: Creating the cookie and then Decrypting it in the next step; Gosh!! 
-				//user=cook.getUserFromPermCookie(idcook);				
-				//Why setting the perm cookie in the session object; This should be set in the response 
+				//FUCP: Creating the cookie and then Decrypting it in the next step; Gosh!!
+				//user=cook.getUserFromPermCookie(idcook);
+				//Why setting the perm cookie in the session object; This should be set in the response
 				//session.setAttribute(Constant.COOKIE,idcook);
 				cook.dropAllCookies(response, user);
 			}else{
@@ -128,7 +109,7 @@ public class LoginActionController extends HttpServlet {
 			if(cmd != null && "login".equalsIgnoreCase(cmd)){
 				Constant.log("Ajax Request for Login Made", 0);
 				try{
-					Gson gson = new GsonBuilder().serializeNulls().create();	
+					Gson gson = new GsonBuilder().serializeNulls().create();
 					response.setStatus(200);
 					String jsondata = gson.toJson(user);
 					out.write(jsondata);
